ea4cd338c7daf90c954f56f17c5fb5bb58c9c4d8
Zack T <MLBZ521@gmail.com>
2017-02-14 14:03:17 -0700

    uninstall_Fonts.sh
    v1.0 = First Production Version

---
+++ Software/uninstall_Fonts.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+###################################################################################################
+# Script Name:  uninstall_Fonts.sh
+# By:  Zack Thompson / Created:  1/27/2017
+# Version:  1.0 / Updated:  1/27/2017 / By:  ZT
+# ChangeLog:
+#	v1.0 = First Production Version
+#
+# Description:  This script deletes all the fonts from the System and Users Fonts folder.
+#
+###################################################################################################
+
+# Delete the following font files from the System Fonts Folder.
+
+rm /Library/Fonts/AkzidGroStdASUBol.otf
+rm /Library/Fonts/AkzidGroStdASUCnd.otf
+rm /Library/Fonts/AkzidGroStdASUIta.otf
+rm /Library/Fonts/AkzidGroStdASULig.otf
+rm /Library/Fonts/AkzidGroStdASULigCnd.otf
+rm /Library/Fonts/AkzidGroStdASUMed.otf
+rm /Library/Fonts/AkzidGroStdASUMedCnd.otf
+rm /Library/Fonts/AkzidGroStdASUMedCndIta.otf
+rm /Library/Fonts/AkzidGroStdASUMedIta.otf
+rm /Library/Fonts/AkzidGroStdASUReg.otf
+rm /Library/Fonts/AkzidGroStdASUBolExt.otf
+rm /Library/Fonts/AkzidGroStdASUExt.otf
+rm /Library/Fonts/AkzidGroStdASULigExt.otf
+rm /Library/Fonts/AkzidGroStdASUMedExt.otf
+
+
+# Delete the following font files from the Users Fonts Folder.
+
+rm ~/Library/Fonts/AkzidGroStdASUBol.otf
+rm ~/Library/Fonts/AkzidGroStdASUCnd.otf
+rm ~/Library/Fonts/AkzidGroStdASUIta.otf
+rm ~/Library/Fonts/AkzidGroStdASULig.otf
+rm ~/Library/Fonts/AkzidGroStdASULigCnd.otf
+rm ~/Library/Fonts/AkzidGroStdASUMed.otf
+rm ~/Library/Fonts/AkzidGroStdASUMedCnd.otf
+rm ~/Library/Fonts/AkzidGroStdASUMedCndIta.otf
+rm ~/Library/Fonts/AkzidGroStdASUMedIta.otf
+rm ~/Library/Fonts/AkzidGroStdASUReg.otf
+rm ~/Library/Fonts/AkzidGroStdASUBolExt.otf
+rm ~/Library/Fonts/AkzidGroStdASUExt.otf
+rm ~/Library/Fonts/AkzidGroStdASULigExt.otf
+rm ~/Library/Fonts/AkzidGroStdASUMedExt.otf
+
+exit 0
