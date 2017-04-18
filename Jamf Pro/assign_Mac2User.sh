5188a73708cee4881de4f89956114bd58559d190
Zack T <MLBZ521@gmail.com>
2017-04-18 14:58:01 -0700

    assign_Mac2User.sh
    v1.0 = First Production Version

---
+++ Jamf Pro/assign_Mac2User.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###################################################################################################
+# Script Name:  assign_Mac2User.sh
+# By:  Zack Thompson / Created:  4/18/2017
+# Version:  1.0 / Updated:  4/18/2017 / By:  ZT
+# ChangeLog:
+#	v1.0 = First Production Version
+#
+# Description:  This script assigns a Mac to a User in JAMF Inventory records for a specific device.
+#
+###################################################################################################
+
+# Use Apple Script to request information via dialog boxes and assign to variables.
+
+endUsername=$(osascript -e 'set userInput to the text returned of (display dialog "Please enter your asurite:" default answer " ")')
+
+realname=$(osascript -e 'set userInput to the text returned of (display dialog "Please enter your full name:" default answer " ")')
+
+email=$(osascript -e 'set userInput to the text returned of (display dialog "Please enter your email address:" default answer " ")')
+
+phone=$(osascript -e 'set userInput to the text returned of (display dialog "Please enter your ASU assigned phone number:" default answer " ")')
+
+
+# Run jamf recon and field it the values gathered above.
+
+sudo jamf recon -endUsername "$endUsername" -realname "$realname" -email "$email" -phone "$phone"
+
+
+exit 0
