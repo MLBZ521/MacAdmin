a026f5641c4ac68aa8a76f12c0d87d3b0a885361
Zack T <MLBZ521@gmail.com>
2017-08-15 13:07:28 -0700

    v1.1 = Cleaned up script just a bit
    + Cleaned up the script just a bit as well as the 'logic'
    + Renamed the script

--- Software/install_DesktopShortcuts.sh
+++ Software/install_DesktopShortcuts.sh
@@ -1,28 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 ###########################################################
-# Script Name:  DesktopShortcuts.sh
+# Script Name:  install_DesktopShortcuts.sh
 # By:  Zack Thompson / Created:  9/4/2015
-# Version:  1.0 / Updated:  9/4/2015 / By:  ZT
+# Version:  1.1 / Updated:  8/15/2017 / By:  ZT
 #
 # Description:  This is a script to deploy Desktop Shortcuts to the logged in user and default users profiles.
 #
 ###########################################################
 
-# Mount Network Share
-sudo mount -t smbfs "//user:password@server/share" /Volumes/share
-
 # Check for local user profiles and then copy files into their Desktop Folders.
-# The if statements just make sure we do not copy to the Guest and Shared Profiles.
-for users in `ls /Users`
+# The if statement is just to make sure we do not copy to the Guest and Shared Profiles.
+for users in $(ls /Users)
 	do
-		if [[ $users = "Guest" ]]; then
-		echo "Minus" $users
-			elif [[ $users = "Shared" ]]; then
-			echo "Minus" $users
-			else
-				sudo -u $users cp /Library/IT_Staging/*.webloc /Users/$users/Desktop/
-			fi
+		if [[ $users != "Guest" || $users != "Shared" ]]; then
+			sudo -u $users cp /Library/IT_Staging/*.webloc /Users/$users/Desktop/
+		fi
 done
 
 # Copy over Desktop Shortcuts for New Users
@@ -31,9 +24,6 @@ sudo cp /Library/IT_Staging/Kronos\ Workforce\ Central.webloc /System/Library/Us
 sudo cp /Library/IT_Staging/Support.webloc /System/Library/User\ Template/English.lproj/Desktop/
 sudo cp /Library/IT_Staging/Website 1.webloc /System/Library/User\ Template/English.lproj/Desktop/
 sudo cp /Library/IT_Staging/Website 2.webloc /System/Library/User\ Template/English.lproj/Desktop/
-
-# Unmount Network Share
-sudo umount /Volumes/share
 
 # Delete all staging files.
 rm /Library/IT_Staging/*
