7122ad3e541d13ed4680dd2a451477489120763b
Zack T <MLBZ521@gmail.com>
2018-01-03 15:01:53 -0700

    v1.1 = Changed to more reliable methods
     + Change the method used to find the SPSS App Bundle
     + Change the method used to inject the location into the installer.properties file
     + Added a line to manually update the App Bundle Version String (this is not done in IBM's update binary)

--- Software/SPSS Statistics/update_SPSS.sh
+++ Software/SPSS Statistics/update_SPSS.sh
@@ -3,9 +3,9 @@
 ###################################################################################################
 # Script Name:  update_SPSS.sh
 # By:  Zack Thompson / Created:  11/22//2017
-# Version:  1.0 / Updated:  11/22/2017 / By:  ZT
+# Version:  1.1 / Updated:  11/27/2017 / By:  ZT
 #
-# Description:  This script grab the current location of the SPSSStatistics.app and inject it into the installer.properties file and then will upgrade an SPSS Installation.
+# Description:  This script grabs the current location of the SPSSStatistics.app and injects it into the installer.properties file and then will upgrade an SPSS Installation.
 #
 ###################################################################################################
 
@@ -16,15 +16,17 @@
 # Set working directory
 	pkgDir=$(/usr/bin/dirname $0)
 # Get the location of SPSSStatistics.app
-	$installLocation=$(/usr/sbin/system_profiler -detailLevel Mini SPApplicationsDataType | /usr/bin/awk -F 'Location: ' '{print $2}' | /usr/bin/grep /Applications | /usr/bin/grep SPSSStatistics.app)
+	installLocation=$(/usr/bin/find /Applications -name *.app | /usr/bin/grep -E "(24\/)?(SPSS) ?(Statistics) ?(24)?(.app)" | /usr/bin/grep -Ev "(.app/|Python)")
 
 # Inject the location to the installer.properties file
-	/usr/bin/awk '/USER_INSTALL_DIR=/ {print; print $installLocation;next}1' installer.properties
+	LANG=C /usr/bin/sed -i '' 's,USER_INSTALL_DIR=,&'"$installLocation"',' $pkgDir/installer.properties
 
 # Silent upgrade using information in the installer.properties file
 	/bin/echo "Upgrading SPSS..."
 		$pkgDir/SPSS_Statistics_Installer_Mac_Patch.bin -f $pkgDir/installer.properties
 	/bin/echo "Upgrade complete!"
+	
+	defaults write /Applications/SPSS/Statistics/24/SPSSStatistics.app/Contents/Info.plist CFBundleShortVersionString 24.0.0.2
 
 /bin/echo "**************************************************"
 /bin/echo 'PostInstall Upgrade Script Finished'
