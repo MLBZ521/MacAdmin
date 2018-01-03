0ed3a1df130e8cea80095c7ef432a10a1566a8c8
Zack T <MLBZ521@gmail.com>
2018-01-03 14:50:05 -0700

    v1.1 = Added Jamf Policy call to install the JDK first
     + Added Jamf Policy call to install the JDK first

--- Software/SPSS Statistics/install_SPSS.sh
+++ Software/SPSS Statistics/install_SPSS.sh
@@ -3,7 +3,7 @@
 ###################################################################################################
 # Script Name:  install_SPSS.sh
 # By:  Zack Thompson / Created:  11/1/2017
-# Version:  1.0 / Updated:  11/1/2017 / By:  ZT
+# Version:  1.1 / Updated:  11/2/2017 / By:  ZT
 #
 # Description:  This script silently installs SPSS.
 #
@@ -13,11 +13,18 @@
 /bin/echo 'Starting PostInstall Script'
 /bin/echo "**************************************************"
 
+# Install prerequisite:  Java JDK
+/bin/echo "Installing prerequisite Java JDK from Jamf..."
+/usr/local/bin/jamf policy -id 721
+
 # Set working directory
 pkgDir=$(/usr/bin/dirname $0)
 
 # Silent install using information in the installer.properties file
+/bin/echo "Installing SPSS..."
 ./SPSS_Statistics_Installer.bin -f ./installer.properties
+
+/bin/echo "Install complete!"
 
 /bin/echo "**************************************************"
 /bin/echo 'PostInstall Script Finished'
