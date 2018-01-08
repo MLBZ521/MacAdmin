f70f14db57d1ff63d7d8eaf1dd29159e68145ef5
Zack T <MLBZ521@gmail.com>
2018-01-08 13:14:39 -0700

    v1.3 = Feeding in the version being updated
     + Feeding in the version being updated from build_SPSS.sh instead of hardcoding it

--- Software/SPSS Statistics/update_SPSS.sh
+++ Software/SPSS Statistics/update_SPSS.sh
@@ -3,7 +3,7 @@
 ###################################################################################################
 # Script Name:  update_SPSS.sh
 # By:  Zack Thompson / Created:  11/22//2017
-# Version:  1.2 / Updated:  12/13/2017 / By:  ZT
+# Version:  1.3 / Updated:  1/8/2018 / By:  ZT
 #
 # Description:  This script grabs the current location of the SPSSStatistics.app and injects it into the installer.properties file and then will upgrade an SPSS Installation.
 #
@@ -13,11 +13,14 @@
 /bin/echo 'Starting PostInstall Upgrade Script'
 /bin/echo "**************************************************"
 
+# Major Version that's being updated (this will be set by the build_SPSS.sh script)
+	version=
+
 # Set working directory
 	pkgDir=$(/usr/bin/dirname $0)
 
 # Get the location of SPSSStatistics.app
-	fullPath=$(/usr/bin/find /Applications -name *.app | /usr/bin/grep -E "(24\/)?(SPSS) ?(Statistics) ?(24)?(.app)" | /usr/bin/grep -Ev "(.app/|Python)")
+	fullPath=$(/usr/bin/find /Applications -name *.app | /usr/bin/grep -E "(${version}\/)?(SPSS) ?(Statistics) ?(${version})?(.app)" | /usr/bin/grep -Ev "(.app/|Python)")
 
 # Get the *.app name
 	appName=$(echo $fullPath | awk -F "/" '{print $NF}')
@@ -36,7 +39,7 @@
 		$pkgDir/SPSS_Statistics_Installer_Mac_Patch.bin -f $pkgDir/installer.properties
 	/bin/echo "Upgrade complete!"
 
-# Inject the proper version into the Info.plist file
+# Inject the proper version into the Info.plist file -- this may not be required for every version; specifically v24.0.0.2, it was needed
 	/usr/bin/sed -i '' 's/24.0.0.1/24.0.0.2/' $fullPath/Contents/Info.plist
 
 /bin/echo "**************************************************"
