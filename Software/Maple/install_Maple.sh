74df06c9fe30c47535ca927a8b424452bd10a6fa
Zack T <MLBZ521@gmail.com>
2018-01-08 22:07:38 -0700

    v1.1 = Updated for Maple 2017
     + Updated for Maple 2017

--- Software/Maple/install_Maple.sh
+++ Software/Maple/install_Maple.sh
@@ -3,17 +3,17 @@
 ###################################################################################################
 # Script Name:  install_Maple.sh
 # By:  Zack Thompson / Created:  3/2/2017
-# Version:  1.0 / Updated:  3/2/2017 / By:  ZT
+# Version:  1.1 / Updated:  6/23/2017 / By:  ZT
 #
-# Description:  This script installs silent installs and activates Maple 2016.0 with a network license.
+# Description:  This script installs silent installs and activates Maple with a network license.
 #
 ###################################################################################################
 
 # Define working directory
-cd /tmp/Maple2016.0
+cd /tmp/Maple2017.0
 
 # Install Maple via built-in script and option file. 
-./Maple2016.0MacInstaller.app/Contents/MacOS/installbuilder.sh --optionfile ./installer.properties
+./Maple2017.0MacInstaller.app/Contents/MacOS/installbuilder.sh --optionfile ./installer.properties
 Echo 'Maple has been installed!'
 
 # Apple update 'Java for OS X 2015-001' is required for Maples as well, installing that here.
