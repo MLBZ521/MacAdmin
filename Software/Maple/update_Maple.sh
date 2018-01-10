f58f5d0ed758d9f0b8b6aaa87d555f2d7aa124be
Zack T <MLBZ521@gmail.com>
2018-01-09 19:36:51 -0700

    v1.2 = Updated for new build process and more
     + Updated for new build process
     + Changed install arugment option
     - No longer using the installer.properties option file

--- Software/Maple/update_Maple.sh
+++ Software/Maple/update_Maple.sh
@@ -3,16 +3,36 @@
 ###################################################################################################
 # Script Name:  update_Maple.sh
 # By:  Zack Thompson / Created:  3/2/2017
-# Version:  1.1 / Updated:  4/4/2017 / By:  ZT
+# Version:  1.2 / Updated:  1/8/2018 / By:  ZT
 #
-# Description:  This script installs silent installs Maple 2016 patches.
+# Description:  This script silently installs a Maple update.
 #
 ###################################################################################################
 
-cd /tmp/Maple2016.2
+/bin/echo "*****  Update Maple process:  START  *****"
 
-./Maple2016.2MacUpgrade.app/Contents/MacOS/installbuilder.sh --optionfile /tmp/Maple2016.2/installer.properties
+##################################################
+# Define Variables
 
-Echo 'Maple has been installed!'
+# Set working directory
+	pkgDir=$(/usr/bin/dirname $0)
+# Version that's being updated (this will be set by the build_Maple.sh script)
+	version=
+
+##################################################
+# Bits staged...
+
+# Install Maple
+/bin/echo "Installing Maple..."
+	"${pkgDir}/Maple${version}MacInstaller.app/Contents/MacOS/installbuilder.sh" --mode unattended
+/bin/echo "Install complete!"
+
+# Get the location of Maple.app
+	fullPath=$(/usr/bin/find /Applications -name "Maple *.app" | /usr/bin/grep -Ev ".app/")
+
+# Inject the proper version into the Info.plist file -- this may not be required for every version; but was not done in 2016.0X updates
+	/usr/bin/sed -i '' 's/2016.00/2016.02/' $fullPath/Contents/Info.plist
+
+/bin/echo "*****  Update Maple process:  COMPLETE  *****"
 
 exit 0
