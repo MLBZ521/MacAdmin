90a45e43e413be0e74d6b327478ac7612c0b3646
Zack T <MLBZ521@gmail.com>
2018-01-09 19:32:23 -0700

    v1.1 = Updated for Maple 2016.2
     + Updated for Maple 2016.2

--- Software/Maple/update_Maple.sh
+++ Software/Maple/update_Maple.sh
@@ -3,15 +3,15 @@
 ###################################################################################################
 # Script Name:  update_Maple.sh
 # By:  Zack Thompson / Created:  3/2/2017
-# Version:  1.0 / Updated:  3/2/2017 / By:  ZT
+# Version:  1.1 / Updated:  4/4/2017 / By:  ZT
 #
-# Description:  This script installs silent installs Maple 2016.1.
+# Description:  This script installs silent installs Maple 2016 patches.
 #
 ###################################################################################################
 
-cd /tmp/Maple2016.1
+cd /tmp/Maple2016.2
 
-./Maple2016.1MacUpgrade.app/Contents/MacOS/installbuilder.sh --optionfile /tmp/Maple2016.1/installer.properties
+./Maple2016.2MacUpgrade.app/Contents/MacOS/installbuilder.sh --optionfile /tmp/Maple2016.2/installer.properties
 
 Echo 'Maple has been installed!'
 
