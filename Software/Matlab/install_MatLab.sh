2bf57b6705af6a459c157e4da721c554b6f69d15
Zack T <MLBZ521@gmail.com>
2018-01-11 15:47:47 -0700

    v1.2.1 = Fixed various issues for Matlab 2017b
     + Fixed various issues found after further testing for Matlab 2017b
     - Remove the argument '-mode silent' from install line; added it back to the installer_input file
     + The 'staging' method from the last revision for the network.lic file did not work, as Matlab does not 'look' for it until it's first launch, which, at that time, the network.lic file is gone (it's pointing to the tempoary unpack directory of the pkg)

--- Software/Matlab/install_MatLab.sh
+++ Software/Matlab/install_MatLab.sh
@@ -1,31 +1,42 @@
 #!/bin/sh
 
-###########################################################
+###################################################################################################
 # Script Name:  install_MatLab.sh
 # By:  Zack Thompson / Created:  3/6/2017
-# Version:  1.2 / Updated:  11/27/2017 / By:  ZT
+# Version:  1.2.1 / Updated:  12/6/2017 / By:  ZT
 #
 # Description:  This script is used to install and activate MatLab with a Network License Server.
 #
-###########################################################
+###################################################################################################
 
 /bin/echo "**************************************************"
-/bin/echo 'Starting PostInstall Script'
+/bin/echo "*****        Install process:  START         *****"
 /bin/echo "**************************************************"
 
 # Set working directory
 	pkgDir=$(/usr/bin/dirname $0)
 
-# Inject the location to the installer.properties file
-	LANG=C /usr/bin/sed -i '' 's,licensePath=,&'"${pkgDir}/network.lic"',' ./installer_input.txt
-
 # Install MatLab via built-in script and option file.
-	./Matlab_2017b_Mac/InstallForMacOSX.app/Contents/MacOS/InstallForMacOSX -mode silent -inputFile ./installer_input.txt
+	/bin/echo "Installing MatLab..."
+	./Matlab_2017b_Mac/InstallForMacOSX.app/Contents/MacOS/InstallForMacOSX -inputFile ./installer_input.txt
+	exitStatus=$?
 
-/bin/echo 'MatLab has been installed!'
+if [[ $exitStatus == 0 ]]; then
+	/bin/echo "Install completed successfully!"
+	/bin/echo "Copying over the license..."
+		/bin/cp ${pkgDir}/network.lic /Applications/MATLAB_R2017b.app/licenses/
+	/bin/echo "License copied."
+else
+	/bin/echo "Install failed!"
+	/bin/echo "**************************************************"
+	/bin/echo "*****        Install process:  FAILED        *****"
+	/bin/echo "**************************************************"
+
+	exit 1
+fi
 
 /bin/echo "**************************************************"
-/bin/echo 'PostInstall Script Finished'
+/bin/echo "*****       Install process:  COMPLETE       *****"
 /bin/echo "**************************************************"
 
 exit 0
