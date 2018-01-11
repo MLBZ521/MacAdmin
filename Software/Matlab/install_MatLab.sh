5c559da609e9ea853cac196a878878cc02688655
Zack T <MLBZ521@gmail.com>
2018-01-11 15:39:30 -0700

    v1.2 = Changed staging methods of the install files
     + No longer staging the files on the disk; now are only kept in the tempoary unpacked package 'Scripts' directory
     + Added code to add the location of the of the unpacked license.nic file to the install_input file before installing

--- Software/Matlab/install_MatLab.sh
+++ Software/Matlab/install_MatLab.sh
@@ -3,7 +3,7 @@
 ###########################################################
 # Script Name:  install_MatLab.sh
 # By:  Zack Thompson / Created:  3/6/2017
-# Version:  1.1 / Updated:  11/1/2017 / By:  ZT
+# Version:  1.2 / Updated:  11/27/2017 / By:  ZT
 #
 # Description:  This script is used to install and activate MatLab with a Network License Server.
 #
@@ -14,12 +14,15 @@
 /bin/echo "**************************************************"
 
 # Set working directory
-pkgDir=$(/usr/bin/dirname $0)
+	pkgDir=$(/usr/bin/dirname $0)
+
+# Inject the location to the installer.properties file
+	LANG=C /usr/bin/sed -i '' 's,licensePath=,&'"${pkgDir}/network.lic"',' ./installer_input.txt
 
 # Install MatLab via built-in script and option file.
-./Matlab_2017b_Mac/InstallForMacOSX.app/Contents/MacOS/InstallForMacOSX -mode silent -inputFile ./installer_input.txt
+	./Matlab_2017b_Mac/InstallForMacOSX.app/Contents/MacOS/InstallForMacOSX -mode silent -inputFile ./installer_input.txt
 
-Echo 'MatLab has been installed!'
+/bin/echo 'MatLab has been installed!'
 
 /bin/echo "**************************************************"
 /bin/echo 'PostInstall Script Finished'
