00be3080cbfcc20843035811caab84f2f7047034
Zack T <MLBZ521@gmail.com>
2018-01-11 13:44:57 -0700

    v1.1= Updated for Matlab 2017b
     + Updated for Matlab 2017b

--- Software/Matlab/install_MatLab.sh
+++ Software/Matlab/install_MatLab.sh
@@ -3,18 +3,26 @@
 ###########################################################
 # Script Name:  install_MatLab.sh
 # By:  Zack Thompson / Created:  3/6/2017
-# Version:  1.0 / Updated:  3/6/2017 / By:  ZT
+# Version:  1.1 / Updated:  11/1/2017 / By:  ZT
 #
 # Description:  This script is used to install and activate MatLab with a Network License Server.
 #
 ###########################################################
 
-# Define working directory
-cd /tmp/MatLabR2017a
+/bin/echo "**************************************************"
+/bin/echo 'Starting PostInstall Script'
+/bin/echo "**************************************************"
+
+# Set working directory
+pkgDir=$(/usr/bin/dirname $0)
 
 # Install MatLab via built-in script and option file.
-./Matlab_2017a_Mac/install -mode silent -inputFile /tmp/MatLabR2017a/installer_input.txt
+./Matlab_2017b_Mac/InstallForMacOSX.app/Contents/MacOS/InstallForMacOSX -mode silent -inputFile ./installer_input.txt
 
 Echo 'MatLab has been installed!'
+
+/bin/echo "**************************************************"
+/bin/echo 'PostInstall Script Finished'
+/bin/echo "**************************************************"
 
 exit 0
