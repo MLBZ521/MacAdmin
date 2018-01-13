9176cdf880a590d170eb778a1e29c9e3bfa5b034
Zack T <MLBZ521@gmail.com>
2018-01-13 00:23:48 -0700

    v1.3 = Updated to new install format
     + Updated to new install format
     - Removed license code from install script

--- Software/Matlab/install_MatLab.sh
+++ Software/Matlab/install_MatLab.sh
@@ -3,40 +3,36 @@
 ###################################################################################################
 # Script Name:  install_MatLab.sh
 # By:  Zack Thompson / Created:  3/6/2017
-# Version:  1.2.1 / Updated:  12/6/2017 / By:  ZT
+# Version:  1.3 / Updated:  1/11/2018 / By:  ZT
 #
-# Description:  This script is used to install and activate MatLab with a Network License Server.
+# Description:  This script installs MatLab.
 #
 ###################################################################################################
 
-/bin/echo "**************************************************"
-/bin/echo "*****        Install process:  START         *****"
-/bin/echo "**************************************************"
+/bin/echo "*****  Install Matlab process:  START  *****"
+
+##################################################
+# Define Variables
 
 # Set working directory
 	pkgDir=$(/usr/bin/dirname $0)
+# Version that's being updated (this will be set by the build_Matlab.sh script)
+	version=
 
-# Install MatLab via built-in script and option file.
-	/bin/echo "Installing MatLab..."
-	./Matlab_2017b_Mac/InstallForMacOSX.app/Contents/MacOS/InstallForMacOSX -inputFile ./installer_input.txt
-	exitStatus=$?
+##################################################
+# Bits staged...
 
-if [[ $exitStatus == 0 ]]; then
-	/bin/echo "Install completed successfully!"
-	/bin/echo "Copying over the license..."
-		/bin/cp ${pkgDir}/network.lic /Applications/MATLAB_R2017b.app/licenses/
-	/bin/echo "License copied."
-else
-	/bin/echo "Install failed!"
-	/bin/echo "**************************************************"
-	/bin/echo "*****        Install process:  FAILED        *****"
-	/bin/echo "**************************************************"
+# Install MatLab with an option file.
+/bin/echo "Installing Matlab..."
+	"${pkgDir}/Matlab_${version}_Mac/InstallForMacOSX.app/Contents/MacOS/InstallForMacOSX" -inputFile "${pkgDir}/installer_input.txt"
+exitStatus=$?
 
+if [[ $exitStatus != 0 ]]; then
+	/bin/echo "ERROR:  Install failed!"
+	/bin/echo "*****  Install Matlab process:  FAILED  *****"
 	exit 1
 fi
 
-/bin/echo "**************************************************"
-/bin/echo "*****       Install process:  COMPLETE       *****"
-/bin/echo "**************************************************"
+/bin/echo "*****  Install Matlab process:  COMPLETE  *****"
 
 exit 0
