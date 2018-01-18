d9c547995aace95f67956838861ddc95d3188540
Zack T <MLBZ521@gmail.com>
2018-01-17 23:53:56 -0700

    v1.2 = Updated to match new install scripts
     + Improved script logic
     + Checks for Java JDK before installing

--- Software/SPSS Statistics/install_SPSS.sh
+++ Software/SPSS Statistics/install_SPSS.sh
@@ -3,31 +3,44 @@
 ###################################################################################################
 # Script Name:  install_SPSS.sh
 # By:  Zack Thompson / Created:  11/1/2017
-# Version:  1.1 / Updated:  11/2/2017 / By:  ZT
+# Version:  1.2 / Updated:  1/17/2018 / By:  ZT
 #
 # Description:  This script silently installs SPSS.
 #
 ###################################################################################################
 
-/bin/echo "**************************************************"
-/bin/echo 'Starting PostInstall Script'
-/bin/echo "**************************************************"
+/bin/echo "*****  Install SPSS process:  START  *****"
 
-# Install prerequisite:  Java JDK
-/bin/echo "Installing prerequisite Java JDK from Jamf..."
-/usr/local/bin/jamf policy -id 721
+##################################################
+# Define Variables
 
 # Set working directory
-pkgDir=$(/usr/bin/dirname $0)
+	pkgDir=$(/usr/bin/dirname $0)
+# Java JDK Directory
+	jdkDir="/Library/Java/JavaVirtualMachines"
+
+##################################################
+# Bits staged...
+
+if [[ -n $(/usr/bin/find $jdkDir -iname *.jdk) ]]; then
+	# Install prerequisite:  Java JDK
+	/bin/echo "Installing prerequisite Java JDK from Jamf..."
+	/usr/local/bin/jamf policy -id 721
+fi
 
 # Silent install using information in the installer.properties file
 /bin/echo "Installing SPSS..."
-./SPSS_Statistics_Installer.bin -f ./installer.properties
+	"${pkgDir}/SPSS_Statistics_Installer.bin" -f "${pkgDir}/installer.properties"
+	exitStatus=$?
+/bin/echo "Exit Status:  ${exitStatus}"
+
+if [[ $exitStatus != 0 ]]; then
+	/bin/echo "ERROR:  Install failed!"
+	/bin/echo "*****  Install SPSS process:  FAILED  *****"
+	exit 1
+fi
 
 /bin/echo "Install complete!"
-
-/bin/echo "**************************************************"
-/bin/echo 'PostInstall Script Finished'
-/bin/echo "**************************************************"
+/bin/echo "*****  Install SPSS process:  COMPLETE  *****"
 
 exit 0
