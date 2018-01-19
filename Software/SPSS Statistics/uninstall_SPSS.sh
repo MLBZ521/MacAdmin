9f197ba5fbb1ffe75ba8a7d74d706d608ef15e38
Zack T <MLBZ521@gmail.com>
2018-01-18 23:16:01 -0700

    v1.1 = Improved the search for installed versions
     + Improved the search for installed versions

--- Software/SPSS Statistics/uninstall_SPSS.sh
+++ Software/SPSS Statistics/uninstall_SPSS.sh
@@ -3,24 +3,27 @@
 ###################################################################################################
 # Script Name:  uninstall_SPSS.sh
 # By:  Zack Thompson / Created:  11/1/2017
-# Version:  1.0 / Updated:  11/1/2017 / By:  ZT
+# Version:  1.1 / Updated:  1/18/2018 / By:  ZT
 #
 # Description:  Remove previous version(s) of SPSS from /Applications
 #
 ###################################################################################################
 
-/bin/echo "**************************************************"
-/bin/echo 'Starting PreInstall Script'
-/bin/echo "**************************************************"
+/bin/echo "*****  Uninstall SPSS process:  START  *****"
 
-oldVersions=$(ls /Applications | grep "SPSS")
+# If the machine has multiple SPSS Applications, loop through them...
+/usr/bin/find -E /Applications -iregex ".*[/](SPSS) ?(Statistics) ?(\d\d)?[.]app" -type d -prune | while IFS="\n" read -r appPath; do
 
-for oldVersion in $oldVersions; do
-   /bin/rm -rf "/Applications/${oldVersion}"
+	# Get the App Bundle name
+		appName=$(/bin/echo $appPath | /usr/bin/awk -F "/" '{print $NF}')
+	# Get only the install path
+		installPath=$(/bin/echo $appPath | /usr/bin/awk -F "/$appName" '{print $1}')
+	# Delete the old version
+		/bin/echo "Uninstalling:  ${appPath}"
+		/bin/rm -rf "${installPath}"
+
 done
 
-/bin/echo "**************************************************"
-/bin/echo 'PreInstall Script Finished'
-/bin/echo "**************************************************"
+/bin/echo "*****  Uninstall SPSS process:  COMPLETE  *****"
 
 exit 0
