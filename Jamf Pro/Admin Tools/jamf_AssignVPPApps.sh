c1c669810c967a71e1a1fc0f954a9606807a89c1
Zack T <MLBZ521@gmail.com>
2018-02-20 15:30:40 -0700

    v0.12 = Minor maintenance and cleanup
     + Minor mainteanance and cleanup

--- Jamf Pro/Admin Tools/jamf_AssignVPPApps.sh
+++ Jamf Pro/Admin Tools/jamf_AssignVPPApps.sh
@@ -3,13 +3,13 @@
 ###################################################################################################
 # Script Name:  jamf_AssignVPPApps.sh
 # By:  Zack Thompson / Created:  2/16/2018
-# Version:  0.11 / Updated:  2/19/2018 / By:  ZT
+# Version:  0.12 / Updated:  2/20/2018 / By:  ZT
 #
 # Description:  This script is used to scope groups to VPP Apps.
 #
 ###################################################################################################
 
-/bin/echo "*****  AssignVPPApps process:  START  *****"
+echo "*****  AssignVPPApps process:  START  *****"
 
 ##################################################
 # Define Variables
@@ -28,16 +28,16 @@
 
 getHelp() {
 	echo "
-usage:  jamf_AssignVPPApps.sh [-get | -assign] [-file /path/to/file.csv] [-help]
+usage:  jamf_AssignVPPApps.sh [-get | -assign] [/path/to/file.txt] [-help]
 
 Info:	Get a list of VPP Apps or assign VPP Apps to groups.
 
 Actions:
-	-get		Gets all the VPP Apps from the JSS and exports them to a csv file.
-			Example:  jamf_AssignVPPApps.sh -get output.csv
+	-get		Gets all the VPP Apps from the JSS and exports them to a txt file.
+			Example:  jamf_AssignVPPApps.sh -get output.txt
 
 	-assign		Read the provide file and assigns (scopes) VPP Apps to a Group.
-			Example:  jamf_AssignVPPApps.sh -assign input.csv
+			Example:  jamf_AssignVPPApps.sh -assign input.txt
 
 	-help		Displays this help section.
 			Example:  jamf_AssignVPPApps.sh -help
@@ -48,18 +48,18 @@ Actions:
 getApps() {
 	outFile="${1}"
 
-	/bin/echo "Building list of all Mobile Device App IDs..."
+	echo "Building list of all Mobile Device App IDs..."
 	# GET list of Mobile Device App IDs from the JSS.
 	appIDs=$(/usr/bin/curl "${curlAPI[@]}" GET $mobileApps | xmllint --format - | xpath /mobile_device_applications/mobile_device_application/id 2>/dev/null | LANG=C sed -e 's/<[^/>]*>//g' | LANG=C sed -e 's/<[^>]*>/\'$'\n/g')
 	
 	# Check if the API call was successful or not.
 	exitCode $? notify
 
-	/bin/echo "Adding headers to output file..."
-	echo -e $header >> $outFile
+	echo "Adding headers to output file..."
 	header="\"App ID\"\t\"App Name\"\t\"Auto Install?\"\t\"Auto Deploy\"\t\"Manage App?\"\t\"Remove App?\"\t\"App Site\"\t\"Scope to Group\""
+	echo -e $header >> "${outFile}"
 
-	/bin/echo "Getting Mobile Device App info..."
+	echo "Getting Mobile Device App info..."
 	# For Each ID, get the Name and Site it is assigned too.
 	for appID in $appIDs; do
 		/usr/bin/curl "${curlAPI[@]}" GET ${mobileAppsByID}/${appID}/subset/General | xmllint --format - | xpath '/mobile_device_application/general/id | /mobile_device_application/general/name | /mobile_device_application/general/site/name | /mobile_device_application/general/deployment_type | /mobile_device_application/general/deploy_automatically | /mobile_device_application/general/deploy_as_managed_app | /mobile_device_application/general/remove_app_when_mdm_profile_is_removed' 2>/dev/null | LANG=C sed -e 's/Install Automatically\/Prompt Users to Install/true/g' | LANG=C sed -e 's/Make Available in Self Service/false/g' | LANG=C sed -e 's/<[^/>]*>/\'$'\"/g' | LANG=C sed -e 's/<[^>]*>/\'$'\"\t/g' | LANG=C sed -e 's/\'$'\t[^\t]*$//' >> $outFile
@@ -67,26 +67,24 @@ getApps() {
 		exitCode $?
 	done
 
-	/bin/echo "List has been saved to:  ${outFile}"
+	echo "List has been saved to:  ${outFile}"
 }
 
 # Read in the App IDs and the Group Name to assign to them.
 assignApps() {
 	inputFile="${1}"
-	/bin/echo "Scoping Mobile Device Apps..."
+	echo "Scoping Mobile Device Apps..."
 
 	# Read in the file and assign to variables
 	while IFS=$'\t' read appID appName appSite autoInstall autoDeploy manageApp removeApp scopeGroup; do
-		# echo "$appID"
-		# echo "$groupName"
 
-		# For sake of editing the csv, we're expecting a true or false value to "Auto Install" the App, but we need to reassign this value to what the JSS expects.
+		# For sake of editing the txt, we're expecting a true or false value to "Auto Install" the App, but we need to reassign this value to what the JSS expects.
 		if [[ $autoInstall == "true" ]]; then
 			autoInstall="Install Automatically/Prompt Users to Install"
 		elif [[ $autoInstall == "false" ]]; then
 			autoInstall="Make Available in Self Service"
 		else
-			/bin/echo "Unable to determine the install method for App ID:  ${appID}"
+			echo "Unable to determine the install method for App ID:  ${appID}"
 			continue
 		fi
 
@@ -116,14 +114,12 @@ exitCode() {
 	if [[ $1 != "0" ]]; then
 		if [[ $2 == "notify" ]]; then
 			# Notify only if told too. 
-			/bin/echo " -> An action failed"
-			# exit 1
+			echo " -> An action failed"
 		fi
 	else
 		if [[ $2 == "notify" ]]; then
 			# Notify only if told too. 
-			/bin/echo " -> Success!"
-			# exit 1
+			echo " -> Success!"
 		fi
 	fi
 }
@@ -169,6 +165,5 @@ case $action in
 	;;
 esac
 
-/bin/echo "*****  AssignVPPApps process:  COMPLETE  *****"
-
+echo "*****  AssignVPPApps process:  COMPLETE  *****"
 exit 0
