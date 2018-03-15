57e25f4a753e5f8f872738551b0c7e2020521232
Zack T <MLBZ521@gmail.com>
2018-03-14 22:02:49 -0700

    v1.0 = Production Ready
    By:  ZT
     + Improved logic in script
     + Added additional options

--- Software/license_NVivo.sh
+++ Software/license_NVivo.sh
@@ -3,9 +3,11 @@
 ###################################################################################################
 # Script Name:  license_NVivo.sh
 # By:  Zack Thompson / Created:  3/14/2018
-# Version:  0.1 / Updated:  3/14/2018 / By:  ZT
+# Version:  1.0 / Updated:  3/14/2018 / By:  ZT
 #
 # Description:  This script applies the license for NVivo.
+#
+# Details:  http://techcenter.qsrinternational.com/desktop/nm11/nm11_mass_deployment.htm
 #
 ###################################################################################################
 
@@ -13,20 +15,86 @@ echo "*****  License NVivo process:  START  *****"
 
 ##################################################
 # Define Variables
-NVivoPath="/Applications/NVivo.app/Contents/MacOS/"
+licenseKey="12345-67891-01234-56789-10123"
+NVivoPath="/Applications/NVivo.app/Contents/MacOS"
 NVivoBinary="${NVivoPath}/NVivo"
 licenseFile="${NVivoPath}/license.xml"
 
 ##################################################
-# Bits staged, license software...
+# Define Functions
 
-if [[ ! -x $NVivoBinary ]]; then
-    echo "Error:  NVivo is not properly installed."
-    echo "*****  License NVivo Process:  FAILED  *****"
-    exit 1
-else
+# Initialize license.
+initialize() {
+    echo "Initializing the license..."
+    exitStatus=$("${NVivoBinary}" -initialize "${licenseKey}")
 
-    # Create the License File.
+    if [[ "${exitStatus}" != *"Your license key has been successfully initialized"* ]]; then
+        echo "ERROR:  Failed to initialize key."
+        echo "ERROR Contents:  ${exitStatus}"
+        echo "*****  License NVivo Process:  FAILED  *****"
+        exit 3
+    fi
+}
+
+# Activate license.
+activate() {
+    # Function createLicense
+    createLicense 
+
+    echo "Activating the license..."
+    exitStatus=$("${NVivoBinary}" -activate "${licenseFile}")
+
+    if [[ "${exitStatus}" != *"Your NVivo license has been activated"* ]]; then
+        echo "ERROR:  Failed to activate key."
+        echo "ERROR Contents:  ${exitStatus}"
+        echo "*****  License NVivo Process:  FAILED  *****"
+        exit 4
+    fi
+}
+
+# Extend license.
+extend() {
+    echo "Extending the license..."
+    exitStatus=$("${NVivoBinary}" -extend "${licenseFile}")
+
+    if [[ "${exitStatus}" != *"Your NVivo license has been extended"* ]]; then
+        echo "ERROR:  Failed to extend key."
+        echo "ERROR Contents:  ${exitStatus}"
+        echo "*****  License NVivo Process:  FAILED  *****"
+        exit 5
+    fi
+
+    # Function activate
+    activate
+}
+
+# Replace license.
+replace() {
+    echo "Replacing the license..."
+    # Function deactivate
+    deactivate
+    # Function initialize
+    initialize
+    # Function activate
+    activate
+ }
+
+# Deactivate license.
+deactivate() {
+    echo "Deactivating the license..."
+    exitStatus=$("${NVivoBinary}" -deactivate)
+
+    if [[ "${exitStatus}" != *"Your NVivo license has been deactivated"* ]]; then
+        echo "ERROR:  Failed to deactivate key."
+        echo "ERROR Contents:  ${exitStatus}"
+        echo "*****  License NVivo Process:  FAILED  *****"
+        exit 6
+    fi
+}
+
+# Create the License File.
+createLicense() {
+    echo "Creating the license file..."
     /bin/cat > "${licenseFile}" <<licenseContents
 <?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <Activation>
@@ -48,22 +116,48 @@ else
 </Request>
 </Activation>
 licenseContents
+}
 
-    # Activate NVivo.
-    echo "Applying the NVivo license..."
-    "${NVivoBinary}" -initialize "12345-67891-01234-56789-10123" -activate "${licenseFile}"
-    exitCode=$?
+##################################################
+# Bits staged, license software...
 
-    # Check the 
-    if [[ $exitCode = 0 ]]; then
-        echo "NVivo has been licensed!"
-        echo "*****  License NVivo Process:  COMPLETE  *****"
-    else
-        echo "Exit Code:  ${exitCode}"
-        echo "ERROR:  Failed to activate NVivo."
-        echo "*****  License NVivo Process:  FAILED  *****"
-        exit 2
-    fi
+if [[ ! -x $NVivoBinary ]]; then
+    echo "Error:  NVivo is not properly installed."
+    echo "*****  License NVivo Process:  FAILED  *****"
+    exit 1
+else
+    # Turn on case-insensitive pattern matching
+    shopt -s nocasematch
+
+    # Determine requested task.
+    case "${4}" in
+        "Activate" | "Activation" )
+            initialize
+            activate
+            echo "NVivo has been activated!"
+            ;;
+        "Extend" | "Extension" )
+            extend
+            echo "NVivo has been extended!"
+            ;;
+        "Replace" | "Replacement" )
+            replace
+            echo "NVivo has been replaced!"
+            ;;
+        "Deactivate" | "Deactivation" )
+            deactivate
+            echo "NVivo has been deactivated!"
+            ;;
+        * )
+            echo "ERROR:  Requested task is invalid"
+            echo "*****  License NVivo Process:  FAILED  *****"
+            exit 2
+            ;;
+    esac
+
+    # Turn off case-insensitive pattern matching
+    shopt -u nocasematch
 fi
 
+echo "*****  License NVivo process:  COMPLETE  *****"
 exit 0
