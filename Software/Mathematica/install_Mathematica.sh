496586b14283e5379541e3aeb30e74c02d9294ab
Zack T <MLBZ521@gmail.com>
2018-03-26 23:02:53 -0700

    v1.3 = Added check if application exists
     + Added check if application exists; if so remove it.

--- Software/Mathematica/install_Mathematica.sh
+++ Software/Mathematica/install_Mathematica.sh
@@ -3,28 +3,34 @@
 ###################################################################################################
 # Script Name:  install_Mathematica.sh
 # By:  Zack Thompson / Created:  1/10/2018
-# Version:  1.2 / Updated:  1/31/2018 / By:  ZT
+# Version:  1.3 / Updated:  3/23/2018 / By:  ZT
 #
 # Description:  This script silently installs Mathematica.
 #
 ###################################################################################################
 
-/bin/echo "*****  Install Mathematica process:  START  *****"
+echo "*****  Install Mathematica process:  START  *****"
 
 ##################################################
 # Define Variables
 
 # Set working directory
-	pkgDir=$(/usr/bin/dirname $0)
+	pkgDir=$(/usr/bin/dirname "${0}")
 
 ##################################################
 # Bits staged...
 
-# Install Mathematica
-/bin/echo "Installing Mathematica..."
-	/bin/mv "${pkgDir}/Mathematica.app" /Applications
-/bin/echo "Install complete!"
+# Check if Mathematic is already installed.
+if [[ -e "/Applications/Mathematica.app" ]]; then
+	echo "Mathematica is currently installed, removing..."
+	/bin/rm -rf "/Applications/Mathematica.app"
+fi
 
-/bin/echo "*****  Install Mathematica process:  COMPLETE  *****"
+# Install Mathematica
+echo "Installing Mathematica..."
+	/bin/mv "${pkgDir}/Mathematica.app" /Applications
+echo "Install complete!"
+
+echo "*****  Install Mathematica process:  COMPLETE  *****"
 
 exit 0
