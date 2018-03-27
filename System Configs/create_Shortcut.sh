be100dfb6a566e086d6e771b27c514d148afb611
Zack T <MLBZ521@gmail.com>
2018-03-27 15:46:19 -0700

    v0.2 = Resolved issues related to adding a shortcut to the dock
     + Resolved issues related to adding a shortcut to the dock
     + Minor formatting and cleanup

--- System Configs/create_Shortcut.sh
+++ System Configs/create_Shortcut.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ###################################################################################################
-# Script Name:  jamf_CreateShortcut.sh
+# Script Name:  create_Shortcut.sh
 # By:  Zack Thompson / Created:  3/26/2018
-# Version:  0.1 / Updated:  3/26/2018 / By:  ZT
+# Version:  0.2 / Updated:  3/27/2018 / By:  ZT
 #
 # Description:  This script will create a website shortcut in a specified location with a specified icon.
 #
@@ -13,15 +13,15 @@ echo "*****  Create Shortcut process:  START  *****"
 
 ##################################################
 # Define Variables
+# If using Jamf, change these values to:  4, 5, 6, 7
 	fileName="${1}"
 	URL="${2}"
 	icon="${3}"
 	location="${4}"
 
 # Get the current user
-currentUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
-
-defaultLocation="/Users/${currentUser}/Library/Shortcuts/"
+	currentUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+	defaultLocation="/Users/${currentUser}/Library/Shortcuts"
 
 ##################################################
 # Setup Functions
@@ -33,7 +33,7 @@ usage:  jamf_CreateShortcut.sh <filename> <URL> <icon> <location>
 Info:	This script will create a website shortcut in a specified location with a specified icon.
 
 Parameters:
-	filename	Thise files' \"Display Name\".
+	filename	The files' \"Display Name\".
 
 	URL			The website the shortcut is for.
 
@@ -47,13 +47,13 @@ Parameters:
 # Bits staged...
 
 # Check to make sure all parameters were provided.
-# if [[ -z "${4}" && -z "${5}" && -z "${6}" && -z "${7}"]]; then
-# 	echo "ERROR:  Missing required parameters!"	
-# 	# Function getHelp
-# 	getHelp
-# 	echo "*****  Create Shortcut process:  FAILED  *****"
-# 	exit 1
-# fi
+if [ -z "${fileName}" -a -z "${URL}" -a -z "${icon}" -a -z "${location}" ]; then
+	echo "ERROR:  Missing required parameters!"	
+	# Function getHelp
+	getHelp
+	echo "*****  Create Shortcut process:  FAILED  *****"
+	exit 1
+fi
 
 echo "Provided configuration:"
 echo "Filename:  ${fileName}"
@@ -61,13 +61,12 @@ echo "URL:  ${URL}"
 echo "Icon:  ${icon}"
 echo "Location:  ${location}"
 
-
 # Create the staging directory if it doesn't existing.
 if [[ ! -d "${defaultLocation}" ]]; then
 	/bin/mkdir "${defaultLocation}"
 fi
 
-# Create the file
+# Create the shortcut file.
 echo "Creating the requested shortcut..."
 /usr/bin/printf  '%s\n' "[InternetShortcut]" "URL=${URL}" "" > "${defaultLocation}/${fileName}.url"
 
@@ -79,19 +78,44 @@ if [[ "${icon}" == "http"* ]]; then
 	icon="/tmp/icon.png"
 fi
 
-# Set the icon
+# Set the icon on the shortcut file.
 echo "Adding the requested icon..."
 /usr/bin/python -c "import Cocoa; import sys; Cocoa.NSWorkspace.sharedWorkspace().setIcon_forFile_options_(Cocoa.NSImage.alloc().initWithContentsOfFile_(sys.argv[1].decode('utf-8')), sys.argv[2].decode('utf-8'), 0) or sys.exit(\"Unable to set file icon\")" "${icon}" "${defaultLocation}/${fileName}.url"
 
 # Check where to place the file.
 if [[ "${location}" == "Dock" ]]; then
-	# Add to dock
-	echo "Adding to the dock..."
-	/usr/bin/defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file://${defaultLocation}/${fileName}.url</string><key>_CFURLStringType</key><integer>15</integer></dict><key>file-label</key><string>${fileName}.url</string><key>file-type</key><integer>32</integer></dict><key>tile-type</key><string>file-tile</string></dict>"
-	/usr/bin/killall Dock
+
+	# Setting a variable that holds whether the Dock item already exists or not (if it does, we don't want to unnecessarily edit and kill the Dock).
+	alreadyExists=0
+
+	# Get the number of items in the persistent-others node; then subtract one for Array value notation.
+	indexItem=$(/usr/libexec/PlistBuddy -x -c "Print :persistent-others" "/Users/${currentUser}/Library/Preferences/com.apple.dock.plist" | /usr/bin/xmllint --format - | /usr/bin/xpath 'count(//plist/array/dict)' 2>/dev/null)
+	indexItem=$((indexItem-1))
+
+	# Loop through all the items in the persistent-others node and compare to the new item being added.
+		for ((i=1; i<=$indexItem; ++i)); do
+		indexLabel=$(/usr/libexec/PlistBuddy -c "Print :persistent-others:${i}:tile-data:file-label" "/Users/${currentUser}/Library/Preferences/com.apple.dock.plist")
+		indexData=$(/usr/libexec/PlistBuddy -c "Print :persistent-others:${i}:tile-data:file-data:_CFURLString" "/Users/${currentUser}/Library/Preferences/com.apple.dock.plist")
+
+		# Check if the current indexItem values equal the new items' values.
+		if [[ "${indexLabel}" == "${fileName}" && "${indexData}" == "file://${defaultLocation}/${fileName}.url" ]]; then
+			alreadyExists=1
+			echo "Already exists!"
+		fi
+	done
+
+	# If the new item does not already exist, add it to the Dock.
+	if [[ $alreadyExists == 0 ]]; then
+		echo "Adding to the dock..."
+		fileNameLocation=$(echo "${fileName}" | /usr/bin/sed 's/ /%20/g')
+		/usr/bin/sudo -s -u "${currentUser}" /usr/bin/defaults write "/Users/${currentUser}/Library/Preferences/com.apple.dock" persistent-others -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file://${defaultLocation}/${fileNameLocation}.url</string><key>_CFURLStringType</key><integer>15</integer></dict><key>file-label</key><string>${fileName}</string><key>file-type</key><integer>32</integer></dict><key>tile-type</key><string>file-tile</string></dict>"
+		/usr/bin/killall Dock
+	fi
+
 else
+	# Move file to the specified location if not adding to the Dock.
 	echo "Moving file in place..."
-	/bin/mv "${defaultLocation}" "${location}"
+	/bin/mv "${defaultLocation}/${fileName}.url" "${location}"
 fi
 
 echo "*****  Create Shortcut process:  COMPLETE  *****"
