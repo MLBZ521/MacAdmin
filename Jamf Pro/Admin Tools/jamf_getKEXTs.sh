4c721ef149ad0346b0c0dc9ddc2f504b0e9342f9
Zack T <MLBZ521@gmail.com>
2018-05-02 22:04:05 -0700

    v1.0 = Initial Version
     + Initial Version

---
+++ Jamf Pro/Admin Tools/jamf_getKEXTs.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+###################################################################################################
+# Script Name:  jamf_getKEXTs.sh
+# By:  Zack Thompson / Created: 5/2/2018
+# Version:  1.0 / Updated:  5/2/2018 / By:  ZT
+#
+# Description:  This script asks for an output file and dumps all KEXT Info to the file.
+#
+# 	- Original script created by Richard Purves:  https://www.richard-purves.com/2017/11/09/mdm-and-the-kextpocalypse-2/
+#
+###################################################################################################
+
+echo "*****  jamf_getKEXTs process:  START  *****"
+
+##################################################
+# Define Variables
+outFile=$(/usr/bin/osascript -e 'tell application (path to frontmost application as text)' -e 'return POSIX path of (choose file name with prompt "Provide a file name and location to save:")' -e 'end tell' 2>/dev/null)
+
+# Stop IFS linesplitting on spaces
+OIFS=$IFS
+IFS=$'\n'
+
+##################################################
+# Setup Functions
+
+fileExists() {
+	if [[ ! -e "${1}" && $2 == "create" ]]; then
+		echo "Creating output file at location:  ${1}"
+		/usr/bin/touch "${1}"
+	fi
+}
+
+##################################################
+# Bits Staged
+
+echo "Searching Applications folder"
+applic=($(/usr/bin/find /Applications -name "*.kext"))
+
+echo "Searching Library Extensions folder"
+libext=($(/usr/bin/find /Library/Extensions -name "*.kext" -maxdepth 1))
+
+echo "Searching Library Application Support folder"
+libapp=($(/usr/bin/find /Library/Application\ Support -name "*.kext"))
+
+echo ""
+
+# Merge the arrays together
+results=("${applic[@]}" "${libext[@]}" "${libapp[@]}")
+echo "Number of results:  ${#results[@]}"
+
+if [ ${#results[@]} != "0" ]; then
+	# Function fileExists
+	fileExists "${outFile}" create
+	
+	for (( loop=0; loop<${#results[@]}; loop++ )); do
+		# Get the Team Identifier for the kext
+		teamid=$(/usr/bin/codesign -d -vvvv ${results[$loop]} 2>&1 | /usr/bin/grep "Authority=Developer ID Application:" | /usr/bin/cut -d"(" -f2 | /usr/bin/tr -d ")" )
+
+		# Get the CFBundleIdentifier for the kext
+		bundid=$(/usr/bin/defaults read "${results[$loop]}"/Contents/Info.plist CFBundleIdentifier)
+
+		echo "KEXT:  ${results[$loop]}" >> "${outFile}"
+		echo "Team ID:  $teamid | Bundle ID: $bundid" >> "${outFile}"
+	done
+
+	/usr/bin/osascript -e 'tell application (path to frontmost application as text) to display dialog "'"List has been saved to:  ${outFile}"'" buttons {"OK"}' > /dev/null
+
+else
+	/usr/bin/osascript -e 'tell application (path to frontmost application as text) to display dialog "Either no KEXTs were found or there was a problem." buttons {"OK"}' > /dev/null
+fi
+
+IFS=$OIFS
+echo "*****  jamf_getKEXTs process:  COMPLETE  *****"
+exit 0
