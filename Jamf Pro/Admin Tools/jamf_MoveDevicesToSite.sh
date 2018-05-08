2184e22d3dd47d31bda44a568a0381a6bab37ff9
Zack T <MLBZ521@gmail.com>
2018-05-07 22:17:16 -0700

    v0.3 = Major Logic clean up and improvements
     - Remove all previous logic from script that I used as the base framework
     + Improved the flow and logic within the script, correctly verbiage and checks

--- Jamf Pro/Admin Tools/jamf_MoveDevicesToSite.sh
+++ Jamf Pro/Admin Tools/jamf_MoveDevicesToSite.sh
@@ -3,7 +3,7 @@
 ###################################################################################################
 # Script Name:  jamf_MoveDevicesToSite.sh
 # By:  Zack Thompson / Created: 4/19/2018
-# Version:  0.2 / Updated:  4/25/2018 / By:  ZT
+# Version:  0.3 / Updated:  5/2/2018 / By:  ZT
 #
 # Description:  This script allows Site Admins to move devices between Sites that they have perms to.
 #
@@ -13,77 +13,35 @@ echo "*****  MoveDevicesToSite process:  START  *****"
 
 ##################################################
 # Define Variables
-	# Either hard code or prompt for credentials
-	# jamfAPIUser="APIUsername"
-	# jamfAPIPassword="APIPassword"
-	
-	jamfPS="https://newjss.company.com:8443"
-	#usersByName="${jamfPS}/JSSResource/users/name"
-	apiGetToken="${jamfPS}/uapi/auth/tokens"
-	apiGetSites="${jamfPS}/uapi/auth/current"
-	computersbyID="${jamfPS}/JSSResource/computers/id"
-	mobileDevicesByID="${jamfPS}/JSSResource/mobiledevices/id"
 
-	# Either use CLI arguments or prompt for choice
-	if [[ "${4}" == "Jamf" ]]; then
-		action=$(/usr/bin/osascript -e 'tell application (path to frontmost application as text)' -e 'set availableActions to {"Computers", "Mobile Devices"}' -e 'set Action to choose from list availableActions with prompt "Which device type do you want to move?" default items {"Computers"}' -e 'end tell' 2>/dev/null)
-		ranBy="Jamf"
-		#siteAdmin="${3}"
-		jamfAPIUser=$(DecryptString $5 'Salt' 'Passphrase')
-		jamfAPIPassword=$(DecryptString $6 'Salt' 'Passphrase')
-	else
-		action="${1}"
-		switch1="${2}"
-		ranBy="CLI"
-		jamfAPIUser=$(/usr/bin/osascript -e 'set userInput to the text returned of (display dialog "Enter your Jamf Username:" default answer "")' 2>/dev/null)
-		jamfAPIPassword=$(/usr/bin/osascript -e 'set userInput to the text returned of (display dialog "Enter your Jamf Password:" default answer "" with hidden answer)' 2>/dev/null)
-	fi
+# JPS URL
+jamfPS="https://newjss.company.com:8443"
+apiGetToken="${jamfPS}/uapi/auth/tokens"
+apiGetSites="${jamfPS}/uapi/auth/current"
+computersbyID="${jamfPS}/JSSResource/computers/id"
+mobileDevicesByID="${jamfPS}/JSSResource/mobiledevices/id"
 
-	# Add -k (--insecure) to disable SSL verification
-	curlAPI=(--silent --show-error --fail --user "${jamfAPIUser}:${jamfAPIPassword}" --write-out "statusCode:%{http_code}" --output - --header "Content-Type: application/xml" --request)
+# Jamf API User that will have permissions to move devices.
+jamfAPIUser=$(DecryptString $5 'Salt' 'Passphrase')
+jamfAPIPassword=$(DecryptString $6 'Salt' 'Passphrase')
+
+# Site Admin Credentials to get which Sites they have permissions too.
+siteAdminUser=$(/usr/bin/osascript -e 'set userInput to the text returned of (display dialog "Enter your Jamf Username:" default answer "")' 2>/dev/null)
+siteAdminPassword=$(/usr/bin/osascript -e 'set userInput to the text returned of (display dialog "Enter your Jamf Password:" default answer "" with hidden answer)' 2>/dev/null)
+
+# Add -k (--insecure) to disable SSL verification
+curlAPI=(--silent --show-error --fail --user "${jamfAPIUser}:${jamfAPIPassword}" --write-out "statusCode:%{http_code}" --output - --header "Content-Type: application/xml" --request)
 
 ##################################################
 # Setup Functions
 
-getHelp() {
-	echo "
-usage:  jamf_MoveDevicesToSite.sh [--computers | --mobile] [/path/to/file.txt] [--help]
-
-Info:	Provide device ID(s) to move to another Site.
-
-Actions:
-	--computers		Gets all the VPP Apps from the JSS and exports them to a txt file.
-			Example:  jamf_MoveDevicesToSite.sh --computers input.list
-
-	--mobile		Read the provide file and assigns (scopes) VPP Apps to a Group.
-			Example:  jamf_MoveDevicesToSite.sh --mobile input.list
-
-	--help		Displays this help section.
-			Example:  jamf_MoveDevicesToSite.sh -help
-"
-}
-
 getSites() {
 	# Create a token based on user provided credentials
-	authToken=$(/usr/bin/curl --silent --show-error --fail --user "${jamfAPIUser}:${jamfAPIPassword}" --output - --header "Accept: application/json" --request POST ${apiGetToken} | python -c "import sys,json; print json.load(sys.stdin)['token']")
+	authToken=$(/usr/bin/curl --silent --show-error --fail --user "${siteAdminUser}:${siteAdminPassword}" --output - --header "Accept: application/json" --request POST ${apiGetToken} | python -c "import sys,json; print json.load(sys.stdin)['token']")
 
 	echo "Getting a list of Sites..."
 	# GET list of Sites that user has permissions to.
-	# curlReturn="$(/usr/bin/curl "${curlAPI[@]}" GET ${usersByName}/${siteAdmin})"
 	getSites="$(/usr/bin/curl --silent --show-error --fail --output - --header "Content-Type: application/json" --header "Authorization: jamf-token ${authToken}" --request POST ${apiGetSites} | python -c 'import sys,json; print "\n".join( [i["name"] for i in json.loads( sys.stdin.read() )["sites"]] )')"
-
-
-	# Check if the API call was successful or not.
-	# curlCode=$(echo "$curlReturn" | /usr/bin/awk -F statusCode: '{print $2}')
-	# if [[ $curlCode != "200" ]]; then
-	# 	informBy "ERROR:  API call failed with error:  ${curlCode}!"
-	# 	echo "*****  MoveDevicesToSite process:  FAILED  *****"
-	# 	exit 4
-	# fi
-
-	# Regex down to just the Site names
-	# adminSites=$(echo "$curlReturn" | /usr/bin/sed -e 's/statusCode\:.*//g' | /usr/bin/xmllint --format - | /usr/bin/xpath /user/sites/site/name 2>/dev/null | LANG=C /usr/bin/sed -e 's/<[^/>]*>//g' | LANG=C /usr/bin/sed -e 's/<[^>]*>/\'$'\n/g')
-
 
 	# Set the osascript parameters and prompt User for Printer Selection.
 	promptForChoice="tell application (path to frontmost application as text) to choose from list every paragraph of \"$getSites\" with prompt \"Choose Site to move device(s) too:\" OK button name \"Select\" cancel button name \"Cancel\""
@@ -95,7 +53,6 @@ getSites() {
 		createAnother="button returned:No"
 		return
 	fi
-
 }
 
 getComputers() {
@@ -116,29 +73,29 @@ checkStatusCode() {
 	case $1 in
 		200 )
 			# Turn off success notifications
-			# informBy " -> Request successful"
+			# inform " -> Request successful"
 		;;
 		201)
 			# Turn off success notifications
-			# informBy "App ID:  ${appID} -> Request to create or update object successful"
+			# inform "Device ID:  ${2} -> Request to create or update object successful"
 		;;
 		400)
-			informBy "App ID:  ${appID} -> Bad request. Verify the syntax of the request specifically the XML body."
+			inform "${deviceType} ID:  ${2} -> Bad request. Verify the syntax of the request specifically the XML body."
 		;;
 		401)
-			informBy "App ID:  ${appID} -> Authentication failed. Verify the credentials being used for the request."
+			inform "${deviceType} ID:  ${2} -> Authentication failed. Verify the credentials being used for the request."
 		;;
 		403)
-			informBy "App ID:  ${appID} -> Invalid permissions. Verify the account being used has the proper permissions for the object/resource you are trying to access."
+			inform "${deviceType} ID:  ${2} -> Invalid permissions. Verify the account being used has the proper permissions for the object/resource you are trying to access."
 		;;
 		404)
-			informBy "App ID:  ${appID} -> Object/resource not found. Verify the URL path is correct."
+			inform "${deviceType} ID:  ${2} -> Object/resource not found. Verify the URL path is correct."
 		;;
 		409)
-			informBy "App ID:  ${appID} -> Conflict"
+			inform "${deviceType} ID:  ${2} -> Conflict"
 		;;
 		500)
-			informBy "App ID:  ${appID} -> Internal server error. Retry the request or contact Jamf support if the error is persistent."
+			inform "${deviceType} ID:  ${2} -> Internal server error. Retry the request or contact Jamf support if the error is persistent."
 		;;
 	esac
 }
@@ -148,39 +105,32 @@ fileExists() {
 		echo "Creating output file at location:  ${1}"
 		/usr/bin/touch "${1}"
 	elif  [[ ! -e "${1}" && $2 == "trip" ]]; then
-		informBy "ERROR:  Unable to find the input file!"
+		inform "ERROR:  Unable to find the input file!"
 		echo "*****  MoveDevicesToSite process:  FAILED  *****"
 		exit 3
 	fi
 }
 
-informBy() {
-	case $ranBy in
-		Jamf )
-			/usr/bin/osascript -e 'tell application (path to frontmost application as text) to display dialog "'"${1}"'" buttons {"OK"}' > /dev/null
-		;;
-		CLI )
-			echo "${1}"
-		;;
-	esac
+inform() {
+	/usr/bin/osascript -e 'tell application (path to frontmost application as text) to display dialog "'"${1}"'" buttons {"OK"}' > /dev/null
 }
 
 ##################################################
 # Bits Staged
 
 # Verify credentials were provided.
-if [[ -z "${jamfAPIUser}" && -z "${jamfAPIPassword}" ]]; then
-	informBy "Jamf credentials are required!"
+if [[ -z "${siteAdminUser}" && -z "${siteAdminPassword}" ]]; then
+	inform "Site Admin credentials are required!"
 	exit 1
 else
 	# Verify credentials that were provided by doing an API call and checking the result to verify permissions.
 	echo "Verifying API credentials..."
-	curlReturn="$(/usr/bin/curl $jamfPS/JSSResource/jssuser -i --silent --show-error --fail --user "${jamfAPIUser}:${jamfAPIPassword}" --write-out "statusCode:%{http_code}")"
+	curlReturn="$(/usr/bin/curl $jamfPS/JSSResource/jssuser -i --silent --show-error --fail --user "${siteAdminUser}:${siteAdminPassword}" --write-out "statusCode:%{http_code}")"
 
 	# Check if the API call was successful or not.
 	curlCode=$(echo "$curlReturn" | /usr/bin/awk -F statusCode: '{print $2}')
 	if [[ $curlCode != *"200"* ]]; then
-		informBy "ERROR:  Invalid API credentials provided!"
+		inform "ERROR:  Invalid Site Admin credentials provided!"
 		echo "*****  MoveDevicesToSite process:  FAILED  *****"
 		exit 2
 	fi
@@ -188,41 +138,31 @@ else
 	echo "API Credentials Valid -- continuing..."
 fi
 
-case $action in
-	--computers | -c | "Computers" )
-		if [[ -n "${switch1}" ]]; then
-			outFile="${switch1}"
-			# Function getApps
-				getApps
-		else
-			outFile=$(/usr/bin/osascript -e 'tell application (path to frontmost application as text)' -e 'return POSIX path of (choose file name with prompt "Provide a file name and location to save the configuration file:")' -e 'end tell' 2>/dev/null)
-			# Function fileExists
-				fileExists "${outFile}" create
-			# Function getApps
-				getApps
-		fi
+# Find out what device type we want to do move.
+deviceType=$(/usr/bin/osascript -e 'tell application (path to frontmost application as text)' -e 'set availableActions to {"Computer", "Mobile Device"}' -e 'set Action to choose from list availableActions with prompt "Which device type do you want to move?" default items {"Computer"}' -e 'end tell' 2>/dev/null)
+
+# Provide list of devices or single IDs?
+methodType=$(/usr/bin/osascript -e 'tell application (path to frontmost application as text)' -e 'set availableActions to {"List", "Single"}' -e 'set Action to choose from list availableActions with prompt "Do you want to move a single device or provide a list of devices?" default items {"Single"}' -e 'end tell' 2>/dev/null)
+
+# Get device IDs
+if [[ $methodType == "Single" ]]; then
+	# Prompt to enter Device ID
+	devices=$()
+else
+	# Prompt for csv file of IDs
+	devices=$(/usr/bin/osascript -e 'tell application (path to frontmost application as text)' -e 'return POSIX path of(choose file with prompt "Select file:" of type {"csv", "txt"})' -e 'end tell' 2>/dev/null)
+fi
+
+# Function getSites
+getSites
+
+case $deviceType in
+	"Computer" )
 	;;
-	--mobile | -m | "Mobile Devices" )
-		if [[ -n "${switch1}" ]]; then
-			inputFile="${switch1}"
-			# Function fileExists
-				fileExists "${inputFile}" trip
-			# Function assignApps
-				assignApps
-		else
-			inputFile=$(/usr/bin/osascript -e 'tell application (path to frontmost application as text)' -e 'return POSIX path of(choose file with prompt "Select configuration file to process:" of type {"txt"})' -e 'end tell' 2>/dev/null)
-			# Function fileExists
-				fileExists "${inputFile}" trip
-			# Function assignApps
-				assignApps
-		fi
-	;;
-	--help | -h | * )
-		# Function getHelp
-		getHelp
+	"Mobile Device" )
 	;;
 esac
 
-informBy "Script successfully completed!"
+inform "Script successfully completed!"
 echo "*****  MoveDevicesToSite process:  COMPLETE  *****"
 exit 0
