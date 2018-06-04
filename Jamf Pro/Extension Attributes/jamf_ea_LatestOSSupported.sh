6f07a61c2700e2d21ac5a58b62c0bbcd1673df7b
Zack T <MLBZ521@gmail.com>
2018-06-04 15:28:44 -0700

    v1.3 = Updated for Mojave
     + Updated for Mojave (Preliminary support)

--- Jamf Pro/Extension Attributes/jamf_ea_LatestOSSupported.sh
+++ Jamf Pro/Extension Attributes/jamf_ea_LatestOSSupported.sh
@@ -3,11 +3,12 @@
 ###################################################################################################
 # Script Name:  jamf_ea_LatestOSSupported.sh
 # By:  Zack Thompson / Created:  9/26/2017
-# Version:  1.2.2 / Updated:  3/15/2018 / By:  ZT
+# Version:  1.3 / Updated:  6/4/2018 / By:  ZT
 #
 # Description:  A Jamf Extension Attribute to check the latest compatible version of macOS.
 #
 #	System Requirements can be found here:  
+#		Mojave - (https://developer.apple.com/)
 #		High Sierra - https://support.apple.com/en-us/HT201475
 #		Sierra - https://support.apple.com/kb/sp742
 #		El Capitan - https://support.apple.com/kb/sp728
@@ -30,10 +31,12 @@
 
 function modelCheck {
 	if [[ $modelMajorVersion -ge $1 && $(/usr/bin/bc <<< "${osVersion} >= 8") -eq 1 ]]; then
+		/bin/echo "<result>Mojave</result>"
+	elif [[ $modelMajorVersion -ge $2 && $(/usr/bin/bc <<< "${osVersion} >= 8") -eq 1 ]]; then
 		/bin/echo "<result>High Sierra</result>"
-	elif [[ $modelMajorVersion -ge $1 && $(/usr/bin/bc <<< "${osVersion} >= 7.5") -eq 1 ]]; then
+	elif [[ $modelMajorVersion -ge $2 && $(/usr/bin/bc <<< "${osVersion} >= 7.5") -eq 1 ]]; then
 		/bin/echo "<result>Sierra</result>"  # (Current OS Limitation, 10.13 Compatible)
-	elif [[ $modelMajorVersion -ge $2 && $(/usr/bin/bc <<< "${osVersion} >= 6.8") -eq 1  ]]; then
+	elif [[ $modelMajorVersion -ge $3 && $(/usr/bin/bc <<< "${osVersion} >= 6.8") -eq 1  ]]; then
 		/bin/echo "<result>El Capitan</result>"
 	else
 		/bin/echo "<result>Model or Current OS Not Supported</result>"
@@ -62,27 +65,27 @@ if [[ $systemRAM -ge $requiredRAM && $systemFreeSpace -ge $requiredFreeSpace ]];
 	case $modelType in
 		"iMac" )
 			# Function modelCheck
-				modelCheck 10 7
+				modelCheck 13 10 7
 			;;
 		"MacBook" )
 			# Function modelCheck
-				modelCheck 6 5
+				modelCheck 8 6 5
 			;;
 		"MacBookPro" )
 			# Function modelCheck
-				modelCheck 7 3
+				modelCheck 9 7 3
 			;;
 		"MacBookAir" )
 			# Function modelCheck
-				modelCheck 3 2
+				modelCheck 5 3 2
 			;;
 		"Macmini" )
 			# Function modelCheck
-				modelCheck 4 3
+				modelCheck 6 4 3
 			;;
 		"MacPro" )
 			# Function modelCheck
-				modelCheck 5 3
+				modelCheck 5 5 3
 			;;
 	esac
 
