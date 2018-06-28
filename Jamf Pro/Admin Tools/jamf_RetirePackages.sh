c26abba68180ac5e721db7b90a66f053642f22cb
Zack T <MLBZ521@gmail.com>
2018-06-27 23:00:50 -0700

    v1.3 = Added additional functionality
     + Added additional functionality, including:  Policies and Computer Groups

--- Jamf Pro/Admin Tools/jamf_RetirePackages.sh
+++ Jamf Pro/Admin Tools/jamf_RetirePackages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ###################################################################################################
-# Script Name:  jamf_RetirePackages.sh
+# Script Name:  jamf_Maintenance.sh
 # By:  Zack Thompson / Created:  9/15/2017
-# Version:  1.2 / Updated:  12/4/2017 / By:  ZT
+# Version:  1.3 / Updated:  6/26/2018 / By:  ZT
 #
 # Description:  This script is used for cleaning up packages in a Jamf Pro Server.
 #
@@ -12,47 +12,131 @@
 ##################################################
 # Define Variables
 	scriptDirectory=$(dirname "${0}")
-	cwd=$(pwd)
-	switch="${1}"
-	input2="${2}"
-	input3="${3}"
-	jamfPS="https://newjss.company.com:8443"
-	jamfURL="${jamfPS}/JSSResource/packages/id"
-	jamfAPIUser="APIUsername"
-	jamfAPIPassword="APIPassword"
+	switch1="${1}"
+	switch2="${2}"
+	switch3="${3}"
+	switch4="${4}"
+	jamfPS="https://jss.company.com:8443"
+	apiPackages="${jamfPS}/JSSResource/packages/id"
+	apiPolicies="${jamfPS}/JSSResource/policies/id"
+	apiComputerGroups="${jamfPS}/JSSResource/computergroups/id"
+	jamfAPIUser=""
+	jamfAPIPassword=""
 
 ##################################################
 # Setup Functions
 
-function getHelp {
+getHelp() {
 echo "
-usage:  jamf_RetirePackages.sh [-packages] [-policies] [-delete] [-help]
+usage:  jamf_RetirePackages.sh --[action] --[type] [file] | [--help]
 
 Info:	Uses jss_helper to query the JSS to get a list of packages and then get a list of policies that are installing the packages.
 
 Actions:
-	-packages	Gets all the packages from the JSS and export them to a file.
-			Example:  jamf_RetirePackages.sh -packages output.file
+	--get | -g		Get all the --[type] provided from the JSS and export them to a file.
+		Example:  jamf_RetirePackages.sh --get --[type] output.file
 
-	-policies	Gets all the policies that install the packages listed in the input file and exports the info to a file.
-			Example:  jamf_RetirePackages.sh -policies input.file output.file
+	--pkgsUsed		Gets all the policies that install each package listed in the input file and exports the info to a file.
+		Example:  jamf_RetirePackages.sh --pkgsUsed input.file output.file
 
-	-delete		Deletes all the packages listed in the input file.
-			Example:  jamf_RetirePackages.sh -delete input.file
+	--delete | -d		Delete all the --[type] provided from the JSS listed in the input file
+		Example:  jamf_RetirePackages.sh --delete --[type] input.file
+
+Types:
+	--packages | -pkgs
+
+	--policies | -p
+
+	--computergroups | -g
+
 "
 }
 
-function getPackages {
+
+getFunction() {
+	case $1 in
+		--packages | -pkgs )
+			if [[ -n "${switch3}" ]]; then
+				# Function fileExists
+				fileExists "${scriptDirectory}" "${switch3}"
+
+				# Function getPackages
+				getPackages "${switch3}"
+			else
+				# Function getHelp
+				getHelp
+			fi
+		;;
+		--policies | -p )
+			if [[ -n "${switch3}" ]]; then
+				echo "Currently get is not supported for Policies."
+			else
+				# Function getHelp
+				getHelp
+			fi
+		;;
+		--computergroups | -g )
+			if [[ -n "${switch3}" ]]; then
+				echo "Currently get is not supported for Computer Groups."
+			else
+				# Function getHelp
+				getHelp
+			fi
+		;;
+		-help | * )
+			# Function getHelp
+			getHelp
+		;;
+	esac
+}
+
+deleteFunction() {
+	case $1 in
+		--packages | -pkgs )
+			if [[ -e "${switch3}" ]]; then
+				# Function deleteObjects
+				deleteObjects "${switch3}" "${apiPackages}"
+			else
+				# Function getHelp
+				getHelp
+			fi
+		;;
+		--policies | -p )
+			if [[ -e "${switch3}" ]]; then
+				# Function deleteObjects
+				deleteObjects "${switch3}" "${apiPolicies}"
+			else
+				# Function getHelp
+				getHelp
+			fi
+		;;
+		--computergroups | -g )
+			if [[ -e "${switch3}" ]]; then
+				# Function deleteObjects
+				deleteObjects "${switch3}" "${apiComputerGroups}"
+			else
+				# Function getHelp
+				getHelp
+			fi
+		;;
+		-help | * )
+			# Function getHelp
+			getHelp
+		;;
+	esac
+}
+
+getPackages() {
 	packages=$(jss_helper package)
 
 	printf '%s\n' "$packages" | while IFS= read -r line; do
 		packageID=$(echo "$line" | awk -F 'ID: ' '{print $2}' | awk -F ' ' '{print $1}')
 		packageName=$(echo "$line" | awk -F 'NAME: ' '{print $2}')
-		echo "$packageID,$packageName" >> "${input2}"
+		echo "$packageID,$packageName" >> "${1}"
 	done
 }
 
-function getPolicies {
+getPkgPolicies() {
 	while IFS= read -r line; do
 		if [[ -n $line ]]; then
 			packageID=$(echo "$line" | awk -F ',' '{print $1}')
@@ -72,75 +156,71 @@ function getPolicies {
 	done < "${1}"
 }
 
-function deletePackages {
+deleteObjects() {
 	while IFS= read -r line; do
 		if [[ -n $line ]]; then
-			packageID=$(echo "$line" | awk -F ',' '{print $1}')
-			packageName=$(echo "$line" | awk -F ',' '{print $2}')
+			itemID=$(echo "$line" | awk -F ',' '{print $1}')
+			itemName=$(echo "$line" | awk -F ',' '{print $2}')
 
-			/bin/echo "Deleting package:  ${packageName}"
-			exitStatus=$(/usr/bin/curl --silent --show-error --fail --user "${jamfAPIUser}:${jamfAPIPassword}" "${jamfURL}/${packageID}" --request DELETE 2>&1)
+			/bin/echo "Deleting:  ${itemName}"
+			exitStatus=$(/usr/bin/curl --silent --show-error --fail --user "${jamfAPIUser}:${jamfAPIPassword}" "${2}/${itemID}" --request DELETE 2>&1)
 			exitCode=$?
 
 			if [[ $exitCode != 0 ]]; then
-				echo "Attempting to delete ID ${packageID} resulted in:  ${exitStatus}" >> errorLog.txt
+				echo " -> FAILED"
+				echo "Attempting to delete ID ${itemID} resulted in:  ${exitStatus}" >> "${scriptDirectory}/errorLog.txt"
 			fi
 		fi
 	done < "${1}"
 }
 
-function fileExists {
+fileExists() {
 	if [[ ! -e "${1}" ]]; then
-		touch "${1}"
-	elif [[ ! -e "${2}" ]]; then
 		echo "Unable to find the input file!"
-		exit 101
+		exit 1
+	elif [[ ! -e "${2}" ]]; then
+		touch "${1}"
 	fi
 }
 
 ##################################################
 # Find out what we want to do...
 
-case $switch in
-	-packages )
-		if [[ -n "${input2}" ]]; then
-			# Function fileExists 
-			fileExists "${input2}"
-			
-			# Function getPackages
-			getPackages "${input2}"
+case $switch1 in
+	--get | -g )
+		if [[ -n "${switch2}" ]]; then
+			# Function getFunction
+			getFunction "${switch2}"
 		else
 			# Function getHelp
 			getHelp
 		fi
 	;;
-	-policies )
-		if [[ -n "${input2}" || -n "${input3}" ]]; then
+	--pkgsUsed )
+		if [[ -n "${switch2}" || -n "${switch3}" ]]; then
+			# Function fileExists
+			fileExists "${switch2}" "${switch3}"
 
-			# Function fileExists 
-			fileExists "${input3}" "${input2}"
-
-			# Function getPolicies
-			getPolicies "${input2}" "${input3}"
+			# Function getPkgPolicies
+			getPkgPolicies "${switch2}" "${switch3}"
 		else
 			# Function getHelp
 			getHelp
 		fi
 	;;
-	-delete )
-		if [[ -n "${input2}" ]]; then
-			# Function fileExists 
-			# fileExists "${input2}"
-			
-			# Function getPackages
-			deletePackages "${input2}"
+	--delete | -d )
+		if [[ -n "${switch2}" ]]; then
+			# Function deleteFunction
+			deleteFunction "${switch2}"
 		else
 			# Function getHelp
 			getHelp
 		fi
 	;;
-	-help | * )
-		# Function getHelp
-		getHelp
-	;;
+	--help | * )
+			# Function getHelp
+			getHelp
+		;;
 esac
+
+exit 0
