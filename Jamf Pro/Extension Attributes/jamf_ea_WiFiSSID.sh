240a40fee25b704b51bb553b7d2f23e82acc1c56
Zack T <MLBZ521@gmail.com>
2018-10-30 12:48:40 -0700

    v1.1 = Code Improvements
     + Code Improvements

--- Jamf Pro/Extension Attributes/jamf_ea_WiFiSSID.sh
+++ Jamf Pro/Extension Attributes/jamf_ea_WiFiSSID.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ###################################################################################################
-# Script Name:  jamf_ea_GetSSID.sh
+# Script Name:  jamf_ea_WiFiSSID.sh
 # By:  Zack Thompson / Created:  9/26/2017
-# Version:  1.0 / Updated:  9/26/2017 / By:  ZT
+# Version:  1.1 / Updated:  10/30/2018 / By:  ZT
 #
 # Description:  A Jamf Extension Attribute to grab the WiFi SSID.
 #
 ###################################################################################################
 
-wiFiSSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep -w 'AirPort\|SSID' | awk -F ": " '{print $2}')
+wiFiSSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | /usr/bin/grep -w 'AirPort\|SSID' | /usr/bin/awk -F ": " '{print $2}')
 
 if [[ -z $wiFiSSID ]]; then
 	echo "<result>Not Connected</result>"
 else
-	echo "<result>$wiFiSSID</result>"
+	echo "<result>${wiFiSSID}</result>"
 fi
 
 exit 0
