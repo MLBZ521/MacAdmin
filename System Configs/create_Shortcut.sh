39f53aa8d21d641067a1fea155d743af5d9ffc6f
Zack T <MLBZ521@gmail.com>
2018-11-06 11:45:56 -0700

    v1.1.0 = Resolved issue that caused duplicate shortcuts in the Dock
     + Resolved issue that caused duplicate shortcuts in the Dock
     + Corrected the For loop to start at zero (in case the user moves the order of Dock icons -- "Downloads" is "0" by default)
     + Added and structured verbosity

--- System Configs/create_Shortcut.sh
+++ System Configs/create_Shortcut.sh
@@ -3,7 +3,7 @@
 ###################################################################################################
 # Script Name:  create_Shortcut.sh
 # By:  Zack Thompson / Created:  3/26/2018
-# Version:  1.0 / Updated:  4/5/2018 / By:  ZT
+# Version:  1.1.0 / Updated:  11/6/2018 / By:  ZT
 #
 # Description:  This script will create a website shortcut in a specified location with a specified icon.
 #
@@ -56,10 +56,12 @@ if [ -z "${fileName}" -a -z "${URL}" -a -z "${icon}" -a -z "${location}" ]; then
 fi
 
 echo "Provided configuration:"
-echo "Filename:  ${fileName}"
-echo "URL:  ${URL}"
-echo "Icon:  ${icon}"
-echo "Location:  ${location}"
+echo -e "\t Filename:  ${fileName}"
+echo -e "\t URL:  ${URL}"
+echo -e "\t Icon:  ${icon}"
+echo -e "\t Location:  ${location}"
+echo ""
+echo "Building shortcut:"
 
 # Create the staging directory if it doesn't existing.
 if [[ ! -d "${defaultLocation}" ]]; then
@@ -67,24 +69,26 @@ if [[ ! -d "${defaultLocation}" ]]; then
 fi
 
 # Create the shortcut file.
-echo "Creating the requested shortcut..."
+echo -e "\t Creating the requested shortcut..."
 /usr/bin/printf  '%s\n' "[InternetShortcut]" "URL=${URL}" "" > "${defaultLocation}/${fileName}.url"
 
 # If the icon provided is on a web server, download it.
 if [[ "${icon}" == "http"* ]]; then
-	echo "Downloading icon..."
+	echo -e "\t Downloading icon..."
 	/usr/bin/curl --silent --show-error --fail "${icon}"  --output /tmp/icon.png
 	# curlAPI=(--silent --show-error --fail --user --write-out "statusCode:%{http_code}")
 	icon="/tmp/icon.png"
 fi
 
 # Set the icon on the shortcut file.
-echo "Adding the requested icon..."
+echo -e "\t Adding the requested icon to shortcut..."
 /usr/bin/python -c "import Cocoa; import sys; Cocoa.NSWorkspace.sharedWorkspace().setIcon_forFile_options_(Cocoa.NSImage.alloc().initWithContentsOfFile_(sys.argv[1].decode('utf-8')), sys.argv[2].decode('utf-8'), 0) or sys.exit(\"Unable to set file icon\")" "${icon}" "${defaultLocation}/${fileName}.url"
 
+echo ""
 # Check where to place the file.
 case "${location}" in
 	Dock )
+		echo "Determine if requested shortcut exists in the Dock..."
 		# Setting a variable that holds whether the Dock item already exists or not (if it does, we don't want to unnecessarily edit and kill the Dock).
 		alreadyExists=0
 
@@ -93,20 +97,20 @@ case "${location}" in
 		indexItem=$((indexItem-1))
 
 		# Loop through all the items in the persistent-others node and compare to the new item being added.
-			for ((i=1; i<=$indexItem; ++i)); do
+			for ((i=0; i<=$indexItem; ++i)); do
 			indexLabel=$(/usr/libexec/PlistBuddy -c "Print :persistent-others:${i}:tile-data:file-label" "/Users/${currentUser}/Library/Preferences/com.apple.dock.plist")
 			indexData=$(/usr/libexec/PlistBuddy -c "Print :persistent-others:${i}:tile-data:file-data:_CFURLString" "/Users/${currentUser}/Library/Preferences/com.apple.dock.plist")
 
 			# Check if the current indexItem values equal the new items' values.
-			if [[ "${indexLabel}" == "${fileName}" && "${indexData}" == "file://${defaultLocation}/${fileName}.url" ]]; then
+			if [[ "${indexLabel}" == "${fileName}" && "${indexData}" == "file://${defaultLocation}/"$(echo "${fileName}" | /usr/bin/sed 's/ /%20/g')".url" ]]; then
 				alreadyExists=1
-				echo "Already exists!"
+				echo -e "\t Shortcut already exists!"
 			fi
 		done
 
 		# If the new item does not already exist, add it to the Dock.
 		if [[ $alreadyExists == 0 ]]; then
-			echo "Adding to the dock..."
+			echo -e "\t Adding to the dock..."
 			fileNameLocation=$(echo "${fileName}" | /usr/bin/sed 's/ /%20/g')
 			/usr/bin/sudo -s -u "${currentUser}" /usr/bin/defaults write "/Users/${currentUser}/Library/Preferences/com.apple.dock" persistent-others -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file://${defaultLocation}/${fileNameLocation}.url</string><key>_CFURLStringType</key><integer>15</integer></dict><key>file-label</key><string>${fileName}</string><key>file-type</key><integer>32</integer></dict><key>tile-type</key><string>file-tile</string></dict>"
 			/usr/bin/killall Dock
@@ -114,7 +118,7 @@ case "${location}" in
 	;;
 	Desktop )
 		# Move file to the specified location if not adding to the Dock.
-		echo "Moving file in place..."
+		echo "Moving file to the Desktop..."
 		/bin/mv "${defaultLocation}/${fileName}.url" "/Users/${currentUser}/Desktop"
 	;;
 	* )
