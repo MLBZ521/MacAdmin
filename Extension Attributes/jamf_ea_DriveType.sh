c8fb18da50c6d9c97f35aebc749fbf23ca83cd6c
Zack T <MLBZ521@gmail.com>
2018-12-11 16:26:55 -0700

    v1.1.0 = Support for Fusion Drives and systems with multiple drives
    + Better support for systems with multiple drives; It will return the drive type for the boot drive
    + Support for Fusion Drives

--- Extension Attributes/jamf_ea_DriveType.sh
+++ Extension Attributes/jamf_ea_DriveType.sh
@@ -3,12 +3,22 @@
 ###################################################################################################
 # Script Name:  jamf_ea_DriveType.sh
 # By:  Zack Thompson / Created:  10/30/2018
-# Version:  1.0 / Updated:  10/30/2018 / By:  ZT
+# Version:  1.1.0 / Updated:  12/11/2018 / By:  ZT
 #
-# Description:  A Jamf Extension Attribute to grab the Drive Type (i.e. Solid State or Rotational).
+# Description:  A Jamf Extension Attribute to grab the Drive Type (i.e. Solid State, Rotational, or Fusion Drive).
 #
 ###################################################################################################
 
-echo "<result>$(/usr/sbin/system_profiler SPSerialATADataType | /usr/bin/grep "Medium Type" | /usr/bin/sed -e 's/^[Medium\ Type:\ ]*//')</result>"
+# Get the Boot Drive.
+bootDisk=$(/usr/sbin/bless --info --getBoot)
+
+# Check Boot Drive for drive type characteristics.
+if [[ $( /usr/sbin/diskutil info $bootDisk | /usr/bin/awk -F "Fusion Drive:" '{print $2}' | /usr/bin/xargs ) == "Yes" ]]; then
+	echo "<result>Fusion Drive</result>"
+elif [[ $( /usr/sbin/diskutil info $bootDisk | /usr/bin/awk -F "Solid State:" '{print $2}' | /usr/bin/xargs ) == "Yes" ]]; then
+	echo "<result>Solid State</result>"
+else
+	echo "<result>Rotational</result>"
+fi
 
 exit 0
