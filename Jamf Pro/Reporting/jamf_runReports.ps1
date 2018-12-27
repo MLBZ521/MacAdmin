2e301d205ed23d1527a8feeb9a53551e3c4b6875
Zack T <MLBZ521@gmail.com>
2018-12-27 09:11:30 -0700

    v1.1.0 = Fixed several issues
    + Added support for TLS v1.2
    + Fixed some structure issues that didn't match the results returned from the API

--- Jamf Pro/Reporting/jamf_runReports.ps1
+++ Jamf Pro/Reporting/jamf_runReports.ps1
@@ -1,8 +1,8 @@
-﻿<#
+<#
 
 Script Name:  jamf_runReports.ps1
 By:  Zack Thompson / Created:  9/13/2018
-Version:  1.0.0 / Updated:  9/13/2018 / By:  ZT
+Version:  1.1.0 / Updated:  12/27/2018 / By:  ZT
 
 Description:  This script will run advanced searches and export each set of results to a file.
 
@@ -27,6 +27,9 @@ $runMobileDeivceReport = "${jamfPS}/JSSResource/advancedmobiledevicesearches/id"
 $computerReports=83,84,85,86,109,110
 $mobileDeviceReports=87
 
+# Set the session to use TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 # ============================================================
 # Functions
 # ============================================================
@@ -35,8 +38,8 @@ function runReports($deviceType, $IDs, $urlID, $csvLocation) {
     Write-Host "Pulling all ${deviceType} records..."
     ForEach ($ID in $IDs) {
         $objectOf_Devices = Invoke-RestMethod -Uri "${urlID}/${ID}" -Method Get -Headers @{"accept"="application/json"} -Credential $APIcredentials
-        $reportName=$(${objectOf_Devices}."advanced_${deviceType}_search".name) -Replace "\[","" -Replace "\]"," -"
-        $objectOf_Devices."advanced_${deviceType}_search"."${deviceType}s" | ForEach-Object {$_.$deviceType} | Select-Object -Property $($objectOf_Devices."advanced_${deviceType}_search".display_fields.display_field.name | ForEach-Object { $_ -Replace " ","_" -Replace "-","_"}) | Export-Csv -Path "$csvLocation\${reportName}.csv" -Append -NoTypeInformation
+        $reportName = $(${objectOf_Devices}."advanced_${deviceType}_search".name) -Replace "\[","" -Replace "\]"," -"
+        $objectOf_Devices."advanced_${deviceType}_search"."${deviceType}s" | ForEach-Object {$_} | Select-Object -Property $($objectOf_Devices."advanced_${deviceType}_search".display_fields.name | ForEach-Object { $_ -Replace " ","_" -Replace "-","_"}) | Export-Csv -Path "$csvLocation\${reportName}.csv" -Append -NoTypeInformation
     }
     Write-Host "All ${deviceType} records have been processed."
 }
