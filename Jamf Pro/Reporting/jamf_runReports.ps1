66b09d94e6ceb858804c59c882c27d8d3635767d
Zack T <MLBZ521@gmail.com>
2018-12-27 09:53:44 -0700

    v1.1.1 = Mainly updated some verbiage
    + Mainly updated some verbiage
    + Updated the save directory naming

--- Jamf Pro/Reporting/jamf_runReports.ps1
+++ Jamf Pro/Reporting/jamf_runReports.ps1
@@ -2,13 +2,14 @@
 
 Script Name:  jamf_runReports.ps1
 By:  Zack Thompson / Created:  9/13/2018
-Version:  1.1.0 / Updated:  12/27/2018 / By:  ZT
+Version:  1.1.1 / Updated:  12/27/2018 / By:  ZT
 
 Description:  This script will run advanced searches and export each set of results to a file.
 
 #>
 
 Write-Host "jamf_runReports Process:  START"
+Write-Host ""
 
 # ============================================================
 # Define Variables
@@ -24,8 +25,13 @@ $jamfPS = "https://jps.company.com:8443"
 $runComputerReport = "${jamfPS}/JSSResource/advancedcomputersearches/id"
 $runMobileDeivceReport = "${jamfPS}/JSSResource/advancedmobiledevicesearches/id"
 
-$computerReports=83,84,85,86,109,110
-$mobileDeviceReports=87
+# Define the Advanced Search IDs
+$computerAdvancedSearches=83,84,85,86,109,110
+$mobileDeviceAdvancedSearches=87
+
+# Setup Save Directory
+$folderDate=$( Get-Date -UFormat %m-%d-%y )
+$saveDirectory = ( $( Read-Host "Provide directiory to save the report" ) -replace '"' )
 
 # Set the session to use TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -35,13 +41,20 @@ $mobileDeviceReports=87
 # ============================================================
 
 function runReports($deviceType, $IDs, $urlID, $csvLocation) {
-    Write-Host "Pulling all ${deviceType} records..."
+    Write-Host "Pulling all ${deviceType} advanced searches..."
+
     ForEach ($ID in $IDs) {
-        $objectOf_Devices = Invoke-RestMethod -Uri "${urlID}/${ID}" -Method Get -Headers @{"accept"="application/json"} -Credential $APIcredentials
-        $reportName = $(${objectOf_Devices}."advanced_${deviceType}_search".name) -Replace "\[","" -Replace "\]"," -"
-        $objectOf_Devices."advanced_${deviceType}_search"."${deviceType}s" | ForEach-Object {$_} | Select-Object -Property $($objectOf_Devices."advanced_${deviceType}_search".display_fields.name | ForEach-Object { $_ -Replace " ","_" -Replace "-","_"}) | Export-Csv -Path "$csvLocation\${reportName}.csv" -Append -NoTypeInformation
+        # Get the results of the Advanced Search.
+        $objectOf_Report = Invoke-RestMethod -Uri "${urlID}/$ID" -Method Get -Headers @{"accept"="application/json"} -Credential $APIcredentials
+
+        # Get the name of the Advanced Search.
+        $reportName = $(${objectOf_Report}."advanced_${deviceType}_search".name) -Replace "\[","" -Replace "\]"," -"
+
+        # Extract the values of the search criteria and output to a csv.
+        $objectOf_Report."advanced_${deviceType}_search"."${deviceType}s" | ForEach-Object { $_ } | Select-Object -Property $( $objectOf_Report."advanced_${deviceType}_search".display_fields.name | ForEach-Object { $_ -Replace " ","_" -Replace "-","_" } ) | Export-Csv -Path "$csvLocation\${reportName}.csv" -Append -NoTypeInformation
     }
-    Write-Host "All ${deviceType} records have been processed."
+
+    Write-Host "All ${deviceType} advanced searches have been processed."
 }
 
 # ============================================================
@@ -66,11 +79,17 @@ Catch {
 }
 
 Write-Host "API Credentials Valid -- continuing..."
+Write-Host ""
+Write-Host "Saving reports to:  ${saveDirectory}\${folderDate}"
+Write-Host ""
 
-$saveDirectory = ($(Read-Host "Provide directiory to save the report") -replace '"')
+if ( !( Test-Path "${saveDirectory}\${folderDate}") ) {    
+        New-Item -Path "${saveDirectory}\${folderDate}" -ItemType Directory | Out-Null
+}
 
-# Call Device Update function for each device type
-runReports computer $computerReports $runComputerReport "${saveDirectory}"
-runReports mobile_device $MobileDeviceReports $runMobileDeivceReport "${saveDirectory}"
+# Call runReports function for each device type
+runReports computer $computerAdvancedSearches $runComputerReport "${saveDirectory}"
+runReports mobile_device $mobileDeviceAdvancedSearches $runMobileDeivceReport "${saveDirectory}\${folderDate}"
 
+Write-Host ""
 Write-Host "jamf_runReports Process:  COMPLETE"
