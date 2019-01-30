93ef20a2d8ef15a37a057ecfad70a3f0ffa6e578
Zack T <MLBZ521@gmail.com>
2019-01-30 16:42:26 -0700

    v1.1.0 = Updated with more exact logic and verbiage
    + Checks if CS:F is installed
    + Also checking if the device is 10.13.2 or newer, before checking if KEXTs are enabled

--- Extension Attributes/jamf_ea_CrowdStrikeStatus.sh
+++ Extension Attributes/jamf_ea_CrowdStrikeStatus.sh
@@ -3,25 +3,23 @@
 ###################################################################################################
 # Script Name:  jamf_ea_CrowdStrikeStatus.sh
 # By:  Zack Thompson / Created:  1/8/2019
-# Version:  1.0.0 / Updated:  1/8/2019 / By:  ZT
+# Version:  1.1.0 / Updated:  1/30/2019 / By:  ZT
 #
-# Description:  This script gets the configuration of Crowd Strike.
+# Description:  This script gets the configuration of Crowd Strike, if installed.
 #
 ###################################################################################################
 
 echo "Checking the Crowd Strike configuration..."
 
 ##################################################
+# Get the OS Minor.Micro Version
+osMinorMicro=$(/usr/bin/sw_vers -productVersion | /usr/bin/awk -F '.' '{print $2"."$3}')
+
 # Define Variables for each item that we want to check for
 customerID="12345678-90AB-CDEF-1234-567890ABCDEF"
 cloudConnectionState="102"
 # A value of 102 indicates the host is connected directly to the CrowdStrike cloud.
 # A value of 126 indicates the host is connected to the CrowdStrike cloud via a proxy.
-
-# Get the current values for the items we want to check
-csCustomerID=$( /usr/sbin/sysctl -n cs.customerid 2>&1 )
-csCloudConnectionState=$( /usr/sbin/sysctl -n cs.comms.cloud_connection_state 2>&1 )
-kextsEnabled=$( /usr/bin/sqlite3 /var/db/SystemPolicyConfiguration/KextPolicy "select * from kext_policy where team_id='X9E956P446' and allowed='0';" | /usr/bin/wc -l | /usr/bin/xargs )
 
 # Hold statuses
 returnResult=""
@@ -29,25 +27,40 @@ returnResult=""
 ##################################################
 # Bits staged, collect the information...
 
-if [[ "${csCustomerID}" != "${customerID}" ]]; then
-    returnResult+="Invalid Customer ID;" 
-fi
+echo "Checking if Crowd Strike is installed..."
 
-if [[ "${csCloudConnectionState}" != "${cloudConnectionState}" ]]; then
-    returnResult+=" Disconnected State;" 
-fi
+if [[ -e "/Library/CS/falconctl" ]]
 
-if [[ "${kextsEnabled}" != "2" ]]; then
-    returnResult+=" KEXTs are not enabled;" 
-fi
+    # Get the customer ID and compare.
+    csCustomerID=$( /usr/sbin/sysctl -n cs.customerid 2>&1 )
+    if [[ "${csCustomerID}" != "${customerID}" ]]; then
+        returnResult+="Invalid Customer ID;" 
+    fi
 
-##################################################
-# Return any errors or the all good.
+    # Get the connection state and compare.
+    csCloudConnectionState=$( /usr/sbin/sysctl -n cs.comms.cloud_connection_state 2>&1 )
+    if [[ "${csCloudConnectionState}" != "${cloudConnectionState}" ]]; then
+        returnResult+=" Disconnected State;" 
+    fi
 
-if [[ -n "${returnResult}" ]]; then
-	echo "<result>${returnResult%?}</result>"
+    # Check if the OS version is 10.13.2 or newer, if it it, check if the KEXTs are enabled.
+    if [[ $(/usr/bin/bc <<< "${osMinorMicro} >= 13.2") -eq 1 ]]; then
+        kextsEnabled=$( /usr/bin/sqlite3 /var/db/SystemPolicyConfiguration/KextPolicy "select * from kext_policy where team_id='X9E956P446' and allowed='0';" | /usr/bin/wc -l | /usr/bin/xargs )
+
+        if [[ "${kextsEnabled}" != "2" ]]; then
+            returnResult+=" KEXTs are not enabled;" 
+        fi
+    fi
+
+    # Return the EA Value.
+    if [[ -n "${returnResult}" ]]; then
+        echo "<result>${returnResult%?}</result>"
+    else
+        echo "<result>Running</result>"
+    fi
+
 else
-	echo "<result>Running</result>"
+	echo "<result>Not Installed</result>"
 fi
 
 exit 0
