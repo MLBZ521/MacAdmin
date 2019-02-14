eca1bcc34abdf04dc6112e5667bde96051b5c6b9
Zack T <MLBZ521@gmail.com>
2019-02-13 23:32:45 -0700

    v1.3.0 = Added logic to check the last cloud connection
    + Added logic to compare the current date to the last cloud connection.

--- Extension Attributes/jamf_ea_CrowdStrikeStatus.sh
+++ Extension Attributes/jamf_ea_CrowdStrikeStatus.sh
@@ -3,7 +3,7 @@
 ###################################################################################################
 # Script Name:  jamf_ea_CrowdStrikeStatus.sh
 # By:  Zack Thompson / Created:  1/8/2019
-# Version:  1.2.0 / Updated:  2/13/2019 / By:  ZT
+# Version:  1.3.0 / Updated:  2/13/2019 / By:  ZT
 #
 # Description:  This script gets the configuration of Crowd Strike, if installed.
 #
@@ -12,14 +12,18 @@
 echo "Checking the Crowd Strike configuration..."
 
 ##################################################
-# Get the OS Minor.Micro Version
-osMinorMicro=$(/usr/bin/sw_vers -productVersion | /usr/bin/awk -F '.' '{print $2"."$3}')
 
 # Define Variables for each item that we want to check for
 customerID="12345678-90AB-CDEF-1234-567890ABCDEF"
 cloudConnectionState="102"
 # A value of 102 indicates the host is connected directly to the CrowdStrike cloud.
 # A value of 126 indicates the host is connected to the CrowdStrike cloud via a proxy.
+# The number of days before trigger virus definitions are out of date.
+lastConnectedVariance=7
+
+
+# Get the OS Minor.Micro Version
+osMinorMicro=$(/usr/bin/sw_vers -productVersion | /usr/bin/awk -F '.' '{print $2"."$3}')
 
 # Hold statuses
 returnResult=""
@@ -51,12 +55,18 @@ if [[ -e "/Library/CS/falconctl" ]]; then
         csCloudConnectionState=$( /Library/CS/falconctl stats | awk -F "State:" '{print $2}' | xargs )
 
         if [[ $csCloudConnectionState != "connected" ]]; then
-            lastConnected=$( /Library/CS/falconctl stats | grep "Last Established At:" | xargs )
-            returnResult+=" Disconnected State; ${lastConnected};"
+            lastConnected=$( /Library/CS/falconctl stats | awk -F "Last Established At:" '{print $2}' | xargs )
+
+            # Check if the last connected date is older than seven days.
+            if [[ $(date -j -f "%b %d %Y %H:%M:%S" "$(echo "${lastConnected}" | sed 's/,//g; s/ at//g; s/ PM//g')" +"%s" ) -ge $(date -j -v-"$($lastConnectedVariance)"d +"%s") ]]; then
+                returnResult+="Last Connected:  ${lastConnected};"
+            else
+                returnResult+=" Disconnected State;"
+            fi
         fi
     fi
 
-    # Check if the OS version is 10.13.2 or newer, if it it, check if the KEXTs are enabled.
+    # Check if the OS version is 10.13.2 or newer, if it is, check if the KEXTs are enabled.
     if [[ $(/usr/bin/bc <<< "${osMinorMicro} >= 13.2") -eq 1 ]]; then
         # Get how many KEXTs are loaded.
         kextsLoaded=$( /usr/sbin/kextstat | grep "com.crowdstrike" | /usr/bin/wc -l | /usr/bin/xargs )
