ac4b7c06f3e79ccc9573c496c746db5777a5b639
Zack T <MLBZ521@gmail.com>
2019-03-13 23:52:58 -0700

    v1.4.0 = Updated to support changes in Falcon Sensor v4.23.8501
    + Updated to support changes in Falcon Sensor v4.23.8501

--- Jamf Pro/Extension Attributes/jamf_ea_CrowdStrikeStatus.sh
+++ Jamf Pro/Extension Attributes/jamf_ea_CrowdStrikeStatus.sh
@@ -3,9 +3,9 @@
 ###################################################################################################
 # Script Name:  jamf_ea_CrowdStrikeStatus.sh
 # By:  Zack Thompson / Created:  1/8/2019
-# Version:  1.3.0 / Updated:  2/13/2019 / By:  ZT
+# Version:  1.4.0 / Updated:  3/13/2019 / By:  ZT
 #
-# Description:  This script gets the configuration of Crowd Strike, if installed.
+# Description:  This script gets the configuration of the CrowdStrike Falcon Sensor, if installed.
 #
 ###################################################################################################
 
@@ -18,9 +18,9 @@ customerID="12345678-90AB-CDEF-1234-567890ABCDEF"
 cloudConnectionState="102"
 # A value of 102 indicates the host is connected directly to the CrowdStrike cloud.
 # A value of 126 indicates the host is connected to the CrowdStrike cloud via a proxy.
-# The number of days before trigger virus definitions are out of date.
-lastConnectedVariance=7
 
+# The number of days before report device has not connected to the CS Cloud.
+lastConnectedVariance=7
 
 # Get the OS Minor.Micro Version
 osMinorMicro=$(/usr/bin/sw_vers -productVersion | /usr/bin/awk -F '.' '{print $2"."$3}')
@@ -68,15 +68,22 @@ if [[ -e "/Library/CS/falconctl" ]]; then
 
     # Check if the OS version is 10.13.2 or newer, if it is, check if the KEXTs are enabled.
     if [[ $(/usr/bin/bc <<< "${osMinorMicro} >= 13.2") -eq 1 ]]; then
+
+        if [[ $(/usr/bin/bc <<< "${csVersion} < 4.23") -eq 1 ]]; then
+            expectedKEXTs="2"
+        else
+            expectedKEXTs="1"
+        fi
+
         # Get how many KEXTs are loaded.
         kextsLoaded=$( /usr/sbin/kextstat | grep "com.crowdstrike" | /usr/bin/wc -l | /usr/bin/xargs )
 
-        if [[ "${kextsLoaded}" != "2" ]]; then
+        if [[ "${kextsLoaded}" != "${expectedKEXTs}" ]]; then
             # Get how many KEXTS are enabled from Jamf or by the user.
             jamfEnabledKEXTs=$( /usr/libexec/PlistBuddy -c "Print :AllowedKernelExtensions:X9E956P447" /Library/Managed\ Preferences/com.apple.syspolicy.kernel-extension-policy.plist -x | /usr/bin/xpath 'count(//string)' 2>/dev/null )
             userEnabledKEXTs=$( /usr/bin/sqlite3 /var/db/SystemPolicyConfiguration/KextPolicy "select * from kext_policy where team_id='X9E956P446' and allowed='0';" | /usr/bin/wc -l | /usr/bin/xargs )
 
-            if [[ "${jamfEnabledKEXTs}" != "2" && "${userEnabledKEXTs}" != "2" ]]; then
+            if [[ "${jamfEnabledKEXTs}" != "${expectedKEXTs}" && "${userEnabledKEXTs}" != "${expectedKEXTs}" ]]; then
                 returnResult+="KEXTs not loaded or enabled;"
             else
                 returnResult+="KEXTs are not loaded;"
