08ba78462e65ab7a6df9c3d9869c7b5fe61932cc
Zack T <MLBZ521@gmail.com>
2020-06-24 19:21:22 -0700

    v1.1.0 = Added Support for Catalina
    + Added Support for Catalina
    + Added check to verify the file exists just in case

--- Jamf Pro/Extension Attributes/jamf_ea_LastOSUpdateInstalled.py
+++ Jamf Pro/Extension Attributes/jamf_ea_LastOSUpdateInstalled.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python
 """
-###################################################################################################
-# Script Name:  jamf_ea_LastOSUpdateInstalled.py
-# By:  Zack Thompson / Created:  8/23/2019
-# Version:  1.0.0 / Updated:  8/23/2019 / By:  ZT
-#
-# Description:  A Jamf Pro Extension Attribute to pull the last operating system update installed.
-#
-###################################################################################################
+Script Name:  jamf_ea_LastOSUpdateInstalled.py
+By:  Zack Thompson / Created:  8/23/2019
+Version:  1.1.0 / Updated:  6/24/2020 / By:  ZT
+
+Description:  A Jamf Pro Extension Attribute to pull the last operating system update installed.
 """
 
+import os
+import re
 from datetime import datetime
 from dateutil import tz
-import re
 
 try:
     from plistlib import dump as custom_plist_Writer  # For Python 3
@@ -27,38 +25,46 @@ def main():
     from_zone = tz.tzutc()
     to_zone = tz.tzlocal()
     lastUpdate = ''
+    install_history = '/Library/Receipts/InstallHistory.plist'
 
     # Define the updates that we're concerned with
     patternProcessNames = re.compile("(?:macOS Installer)|(?:OS X Installer)|(?:softwareupdated)")
-    patternDisplayName = re.compile("(?:macOS 10\.14\.\d Update)|(?:Install macOS High Sierra)|(?:macOS Sierra Update)|(?:OS X El Capitan Update)|(?:Security Update \d\d\d\d-\d\d\d).*")
+    patternDisplayName = re.compile("(?:macOS Catalina 10\.15\.\d)|(?:macOS 10\.14\.\d Update)|(?:Install macOS High Sierra)|(?:macOS Sierra Update)|(?:OS X El Capitan Update)|(?:Security Update \d\d\d\d-\d\d\d).*")
     patternPackageIdentifiers = re.compile("(?:com\.apple\.pkg\.macOSBrain)|(?:com\.apple\.pkg\.InstallAssistantMAS)")
 
-    # Load the InstallHistory.plist
-    plist_Contents = custom_plist_Reader('/Library/Receipts/InstallHistory.plist')
+    # Verify file exists
+    if os.path.exists(install_history):
 
-    # Loop through the history...
-    for update in reversed(plist_Contents):
-        if patternProcessNames.search(update["processName"]):
-            if patternDisplayName.search(update["displayName"]):
-                for package in update["packageIdentifiers"]:
-                    if patternPackageIdentifiers.search(package):
-                        continue
+        # Load the InstallHistory.plist
+        plist_Contents = custom_plist_Reader(install_history)
 
-                lastUpdate = ''
-                lastUpdate = lastUpdate + str(update["displayName"]).lstrip('Install ')
-                if update["displayVersion"] and update["displayVersion"] != " ":
-                    lastUpdate = lastUpdate + ' ' + update["displayVersion"]
-                if update["date"]:
-                    timeStamp = update["date"]
-                    timeStamp = timeStamp.replace(tzinfo=from_zone)
-                    timeStamp = timeStamp.astimezone(to_zone)
-                    lastUpdate = lastUpdate + ' @ ' + str(timeStamp)
-                break
+        # Loop through the history...
+        for update in reversed(plist_Contents):
+            if patternProcessNames.search(update["processName"]):
+                if patternDisplayName.search(update["displayName"]):
+                    for package in update["packageIdentifiers"]:
+                        if patternPackageIdentifiers.search(package):
+                            continue
 
-    if lastUpdate:
-        print("<result>{}</result>".format(lastUpdate))
+                    lastUpdate = ''
+                    lastUpdate = lastUpdate + str(update["displayName"]).lstrip('Install ')
+                    if update["displayVersion"] and update["displayVersion"] != " ":
+                        lastUpdate = lastUpdate + ' ' + update["displayVersion"]
+                    if update["date"]:
+                        timeStamp = update["date"]
+                        timeStamp = timeStamp.replace(tzinfo=from_zone)
+                        timeStamp = timeStamp.astimezone(to_zone)
+                        lastUpdate = lastUpdate + ' @ ' + str(timeStamp)
+                    break
+
+        if lastUpdate:
+            print("<result>{}</result>".format(lastUpdate))
+        else:
+            print("<result>No Updates Installed</result>")
+
     else:
-        print("<result>No Updates Installed</result>")
+        print("<result>Missing InstallHistory.plist</result>")
+
 
 if __name__ == "__main__":
     main()
