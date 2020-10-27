5d503a3145afd0d859a618928098c8ab60153b13
Zack T <MLBZ521@gmail.com>
2020-10-27 13:29:07 -0700

    v1.1.0 = Updated to support Big Sur
    + Updated to support Big Sur

--- Jamf Pro/Extension Attributes/jamf_ea_RecoveryHD.sh
+++ Jamf Pro/Extension Attributes/jamf_ea_RecoveryHD.sh
@@ -3,16 +3,23 @@
 ###################################################################################################
 # Script Name:  jamf_ea_RecoveryHD.sh
 # By:  Zack Thompson / Created:  10/30/2018
-# Version:  1.0 / Updated:  10/30/2018 / By:  ZT
+# Version:  1.1.0 / Updated:  10/21/2020 / By:  ZT
 #
 # Description:  A Jamf Extension Attribute that displays whether the Recovery Volume is present.
 #
 ###################################################################################################
 
-if [[ -z $(/usr/sbin/diskutil list | /usr/bin/grep -w 'Recovery HD\|APFS Volume Recovery') ]] ; then
+# Get the Boot Drive.
+bootDisk=$( /usr/sbin/bless --info --getBoot )
+
+if [[ -z $( /usr/sbin/diskutil info "${bootDisk}" | /usr/bin/grep 'Recovery' ) ]] ; then
+
 	echo "<result>Not Present</result>"
+
 else
+
 	echo "<result>Present</result>"
+
 fi
 
 exit 0
