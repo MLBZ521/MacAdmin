92a0ec2f6700287409f99b82237098c5e8b79e14
Zack T <MLBZ521@gmail.com>
2020-11-13 00:50:45 -0700

    v1.3.0 = Fixed issue for CS:F Version older than 5.36
    Fixed issue for CS:F Version older than 5.36...all this mainly for 5.34 and macOS 10.13 High Sierra.

--- Jamf Pro/Extension Attributes/jamf_ea_CrowdStrikeVersion.sh
+++ Jamf Pro/Extension Attributes/jamf_ea_CrowdStrikeVersion.sh
@@ -3,7 +3,7 @@
 ###################################################################################################
 # Script Name:  jamf_ea_CrowdStrikeVersion.sh
 # By:  Zack Thompson / Created:  1/8/2019
-# Version:  1.2.0 / Updated:  11/10/2020 / By:  ZT
+# Version:  1.3.0 / Updated:  11/13/2020 / By:  ZT
 #
 # Description:  This script gets the version of Crowd Strike, if installed.
 #
@@ -13,7 +13,6 @@ echo "Checking if Crowd Strike is installed..."
 
 ##################################################
 # Possible falconctl binary locations
-
 falconctl_AppLocation="/Applications/Falcon.app/Contents/Resources/falconctl"
 falconctl_OldLocation="/Library/CS/falconctl"
 
@@ -24,7 +23,24 @@ getFalconctlVersion() {
 
     csAgentInfo=$( "${1}" stats agent_info --plist )
 
-	/usr/libexec/PlistBuddy -c "Print :agent_info:version" /dev/stdin <<< "$( echo ${csAgentInfo} )"
+	csVersion=$( /usr/libexec/PlistBuddy -c "Print :agent_info:version" /dev/stdin <<< "$( echo ${csAgentInfo} )" 2> /dev/null )
+	plistBuddyExitCode=$?
+
+	if [[ $plistBuddyExitCode -ne 0 ]]; then
+
+        # Get the Crowd Strike version from sysctl for versions prior to v5.36.
+        csVersion=$( /usr/sbin/sysctl -n cs.version )
+        csVersionExitCode=$?
+
+        if [[ $csVersionExitCode -ne 0 ]]; then
+
+            csVersion="Not Running"
+
+        fi
+
+    fi
+
+	echo "${csVersion}"
 
 }
 
@@ -34,8 +50,7 @@ getFalconctlVersion() {
 if  [[ -e "${falconctl_AppLocation}" && -e "${falconctl_OldLocation}" ]]; then
 
     # Multiple versions installed
-    echo "<result>ERROR:  Multiple CS Versions installed</result>"
-    exit 0
+    csVersion="ERROR:  Multiple CS Versions installed"
 
 elif  [[ -e "${falconctl_AppLocation}" ]]; then
 
@@ -47,27 +62,9 @@ elif  [[ -e "${falconctl_OldLocation}" ]]; then
 
 else
 
-    echo "<result>Not Installed</result>"
-    exit 0
+    csVersion="Not Installed"
 
 fi
 
-if [[ -z "${csVersion}" ]]; then
-
-	# Get the Crowd Strike version from sysctl for versions prior to v5.36.
-	csVersion=$( /usr/sbin/sysctl -n cs.version )
-	csVersionExitCode=$?
-
-	if [[ $csVersionExitCode -ne 0 ]]; then
-
-		echo "<result>Not Running</result>"
-
-	fi
-
-else
-
-	echo "<result>${csVersion}</result>"
-
-fi
-
+echo "<result>${csVersion}</result>"
 exit 0
