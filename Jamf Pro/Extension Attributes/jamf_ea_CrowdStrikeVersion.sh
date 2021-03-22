423f8a9c6bb679cc22af93f6f849cea6ade5a50a
Zack T <MLBZ521@gmail.com>
2021-03-22 13:57:56 -0700

    v1.3.1 = Code Improvement
    No logic or functionality changes.

--- Jamf Pro/Extension Attributes/jamf_ea_CrowdStrikeVersion.sh
+++ Jamf Pro/Extension Attributes/jamf_ea_CrowdStrikeVersion.sh
@@ -3,7 +3,7 @@
 ###################################################################################################
 # Script Name:  jamf_ea_CrowdStrikeVersion.sh
 # By:  Zack Thompson / Created:  1/8/2019
-# Version:  1.3.0 / Updated:  11/13/2020 / By:  ZT
+# Version:  1.3.1 / Updated:  3/22/2021 / By:  ZT
 #
 # Description:  This script gets the version of Crowd Strike, if installed.
 #
@@ -13,58 +13,58 @@ echo "Checking if Crowd Strike is installed..."
 
 ##################################################
 # Possible falconctl binary locations
-falconctl_AppLocation="/Applications/Falcon.app/Contents/Resources/falconctl"
-falconctl_OldLocation="/Library/CS/falconctl"
+falconctl_app_location="/Applications/Falcon.app/Contents/Resources/falconctl"
+falconctl_old_location="/Library/CS/falconctl"
 
-##################################################
+#############################################n#####
 # Functions
 
-getFalconctlVersion() {
+get_falconctl_version() {
 
     csAgentInfo=$( "${1}" stats agent_info --plist )
 
-	csVersion=$( /usr/libexec/PlistBuddy -c "Print :agent_info:version" /dev/stdin <<< "$( echo ${csAgentInfo} )" 2> /dev/null )
-	plistBuddyExitCode=$?
+	cs_version=$( /usr/libexec/PlistBuddy -c "Print :agent_info:version" /dev/stdin <<< "${csAgentInfo}" 2> /dev/null )
+	plistBuddy_exit_code=$?
 
-	if [[ $plistBuddyExitCode -ne 0 ]]; then
+	if [[ $plistBuddy_exit_code -ne 0 ]]; then
 
         # Get the Crowd Strike version from sysctl for versions prior to v5.36.
-        csVersion=$( /usr/sbin/sysctl -n cs.version )
-        csVersionExitCode=$?
+        cs_version=$( /usr/sbin/sysctl -n cs.version )
+        cs_version_exit_code=$?
 
-        if [[ $csVersionExitCode -ne 0 ]]; then
+        if [[ $cs_version_exit_code -ne 0 ]]; then
 
-            csVersion="Not Running"
+            cs_version="Not Running"
 
         fi
 
     fi
 
-	echo "${csVersion}"
+	echo "${cs_version}"
 
 }
 
 ##################################################
 # Bits staged, collect the information...
 
-if  [[ -e "${falconctl_AppLocation}" && -e "${falconctl_OldLocation}" ]]; then
+if  [[ -e "${falconctl_app_location}" && -e "${falconctl_old_location}" ]]; then
 
     # Multiple versions installed
-    csVersion="ERROR:  Multiple CS Versions installed"
+    cs_version="ERROR:  Multiple CS Versions installed"
 
-elif  [[ -e "${falconctl_AppLocation}" ]]; then
+elif  [[ -e "${falconctl_app_location}" ]]; then
 
-    csVersion=$( getFalconctlVersion "${falconctl_AppLocation}" )
+    cs_version=$( get_falconctl_version "${falconctl_app_location}" )
 
-elif  [[ -e "${falconctl_OldLocation}" ]]; then
+elif  [[ -e "${falconctl_old_location}" ]]; then
 
-    csVersion=$( getFalconctlVersion "${falconctl_OldLocation}" )
+    cs_version=$( get_falconctl_version "${falconctl_old_location}" )
 
 else
 
-    csVersion="Not Installed"
+    cs_version="Not Installed"
 
 fi
 
-echo "<result>${csVersion}</result>"
+echo "<result>${cs_version}</result>"
 exit 0
