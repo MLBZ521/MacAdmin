e15632ecfb4722568c51f6e8f22d8a13291c0e5f
Zack T <MLBZ521@gmail.com>
2022-01-24 13:33:58 -0700

    v1.1.0 = Updated for changes in 11.5
    Updated to support changes with Rosetta 2 in Big Sur 11.5.

--- Jamf Pro/Extension Attributes/jamf_ea_Rosetta2Installed.sh
+++ Jamf Pro/Extension Attributes/jamf_ea_Rosetta2Installed.sh
@@ -3,29 +3,27 @@
 ###################################################################################################
 # Script Name:  jamf_ea_Rosetta2Installed.sh
 # By:  Zack Thompson / Created:  4/5/2021
-# Version:  1.0.0 / Updated:  4/5/2021 / By:  ZT
+# Version:  1.1.0 / Updated:  11/3/2021 / By:  ZT
 #
 # Description:  A Jamf Extension Attribute to determine if Rosetta 2 is installed.
 #
 ###################################################################################################
 
-arch=$( /usr/bin/arch )
+if [[ $( /usr/bin/arch ) == "arm64" ]]; then
 
-if [[ "${arch}" == "arm64" ]]; then
+    if [[ $( /usr/bin/pgrep oahd > /dev/null 2>&1 ) ]]; then
 
-    if [[ -f "/Library/Apple/System/Library/LaunchDaemons/com.apple.oahd.plist" ]]; then
-
-        echo "<result>Installed<result>"
+        echo "<result>Installed</result>"
 
     else
 
-        echo "<result>Not Installed<result>"
+        echo "<result>Not Installed</result>"
 
     fi
 
 else
 
-    echo "<result>Not Compatible<result>"
+    echo "<result>Not Compatible</result>"
 
 fi
 
