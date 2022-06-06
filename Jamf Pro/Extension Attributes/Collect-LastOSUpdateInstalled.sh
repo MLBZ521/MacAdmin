07f074c9f65e6b9b3331455ada566770105c0206
Zack T <MLBZ521@gmail.com>
2022-06-06 16:26:24 -0700

    v1.1.0 = Improved tracking/reporting installed updates
    + Updated to support changes in v1.7.0 of `Get-LastOSUpdateInstalled.py`.
    * Changed how installed updates are tracked and reported.  This prevents devices from submitting inventory multiple times in the same day if it reboots multiple times.
    * Added support for a local EA History which will locally log each value that is collected on inventory.

--- Jamf Pro/Extension Attributes/Collect-LastOSUpdateInstalled.sh
+++ Jamf Pro/Extension Attributes/Collect-LastOSUpdateInstalled.sh
@@ -3,32 +3,57 @@
 ###################################################################################################
 # Script Name:  Collect-LastOSUpdateInstalled.sh
 # By:  Zack Thompson / Created:  3/16/2021
-# Version:  1.0.0 / Updated:  3/16/2021 / By:  ZT
+# Version:  1.1.0 / Updated:  5/20/2022 / By:  ZT
 #
-# Description:  A Jamf Extension Attribute to collect the results of Get-LastOSUpdateInstalled.py.
+# Description:  A Jamf Pro Extension Attribute to collect the results of Get-LastOSUpdateInstalled.py.
 #
 ###################################################################################################
 
+##################################################
+#  Define Variables
+
 local_inventory="/opt/ManagedFrameworks/Inventory.plist"
+local_ea_history="/opt/ManagedFrameworks/EA_History.log"
+
+#############################################n#####
+# Functions
+
+write_to_log() {
+    message="${1}"
+    time_stamp=$( /bin/date +%Y-%m-%d\ %H:%M:%S )
+    echo "${time_stamp}:  ${message}" >> "${local_ea_history}"
+}
+
+##################################################
+# Bits staged, collect the information...
 
 if [[ -e $local_inventory ]]; then
 
-    last_update=$( /usr/bin/defaults read "${local_inventory}" last_os_update_installed )
+    last_update=$( /usr/bin/defaults read "${local_inventory}" "last_os_update_installed" 2> /dev/null )
+    # last_reported_update=$( /usr/bin/defaults read "${local_inventory}" "last_reported_os_update_installed" 2> /dev/null )
 
-    if [[ -n $last_update ]]; then
-    
-        echo "<result>${last_update}</result>"
+    if [[ -z $last_update ]]; then
+
+        report="No updates installed"
 
     else
 
-        echo "<result>No Updates Installed</result>"
+    # if [[ -z $last_reported_update || "${last_reported_update}" == "${last_update}" ]]; then
+
+    /usr/bin/defaults write "${local_inventory}" "last_reported_os_update_installed" "${last_update}"
+
+    # fi
+
+    report="${last_update}"
 
     fi
 
 else
 
-    echo "<result>Missing local inventory register</result>"
+    report="Missing local inventory register"
 
 fi
 
+write_to_log "Last OS Update Installed:  ${report}"
+echo "<result>${report}</result>"
 exit 0
