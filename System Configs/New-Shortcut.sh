427844becdcfc5a0035105420bb35850e790f609
Zack T <MLBZ521@gmail.com>
2023-03-10 22:53:26 -0700

    v1.6.0 = Major refactor & support to replace/remove shortcut
    Major refactor of logic to add support for new functionality:
      * Script can now Replace and Remove existing (matching) Shortcuts
    * Renamed Script; mainly as I left it open to support more shortcut types, but probably should just utilize MDM native functionality instead
    * Moved chunks of code into functions
    - Removed unnecessary code/logic

--- System Configs/New-Shortcut.sh
+++ System Configs/New-Shortcut.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-###################################################################################################
-# Script Name:  New-Shortcut.sh
+####################################################################################################
+# Script Name:  New-InternetShortcut.sh
 # By:  Zack Thompson / Created:  3/26/2018
-# Version:  1.5.0 / Updated:  3/9/2023 / By:  ZT
+# Version:  1.6.0 / Updated:  3/10/2023 / By:  ZT
 #
 # Description:  This script will create an internet shortcut
 # 	in the specified location with the specified icon.
 #
-###################################################################################################
-
-echo -e "*****  Create Shortcut process:  START  *****\n"
+####################################################################################################
 
 ##################################################
 # Define Variables
-
-# If using Jamf Pro, change these values to:  4, 5, 6, 7
-file_name="${1}.url"
-URL="${2}"
-icon="${3}"
-location="${4}"
 
 # Get the current user
 current_user=$( /usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | \
@@ -34,21 +26,66 @@ python_binary="/opt/ManagedFrameworks/Python.framework/Versions/Current/bin/pyth
 
 get_help() {
 echo "
-usage:   New-Shortcut.sh <filename> <URL> <icon> <location>
+usage:   New-InternetShortcut.sh <filename> <URL> <icon> <location> [ <option> ]
 
-Info:	This script will create an internet shortcut \
-in the specified location with the specified icon.
+Info:	The default action is to create an internet shortcut in the specified location \
+with the specified icon.  Optionally, the script can replace or remove the shortcut.
 
 Parameters:
 	filename	The \"Display Name\" of the file.
 
-	URL			The website the shortcut is to open.
+	URL		The website the shortcut is to open.
 
 	icon		The image that will be used for the icon.
 
 	location	Where the shortcut will be saved.
-				Supported options are:   \"Dock\" or \"Desktop\"
+			Supported options are:   [ Dock | Desktop ]
+
+	option	Remove (delete) or replace icon if it exists
+			Supported options are:   [ [ Remove | Delete ] | Replace ]
 "
+}
+
+console_writer() {
+
+	# Writes messages to console even when called via a Function
+
+	local message="${1}"
+	local special="${2}"
+
+	if [[ "${special}" == "-e" ]]; then
+		# Interpret backslash escape sequences
+		echo -e "${message}" >&2
+	elif [[ "${special}" == "-n" ]]; then
+		# Do not print new line
+		echo -n "${message}" >&2
+	else
+		echo "${message}" >&2
+	fi
+
+}
+
+handle_exit() {
+
+	local code="${1}"
+	local message="${2}"
+	local special="${3}"
+
+	if [[ -n "${message}" ]]; then
+		console_writer "${message}" "${special}"
+	fi
+
+	if [[ "${code}" -ne 0 ]]; then
+		# Function get_help
+		get_help
+
+		console_writer "\n*****  Create Shortcut process:  FAILED  *****" "-e"
+		exit "${code}"
+	fi
+
+	console_writer "\n*****  Create Shortcut process:  COMPLETE  *****" "-e"
+	exit 0
+
 }
 
 xpath_tool() {
@@ -61,10 +98,60 @@ xpath_tool() {
 
 }
 
+PlistBuddy_Helper() {
+	# Helper function to interact with plists.
+	# All options utilize the "-c" option (non-interactive mode).
+	# All options except a file path to be passed _except_ for the action "print_stdin"
+
+	# Arguments
+	# $1 = (str) action to perform on the plist; Supported options are:
+		# "print_stdin" - expects to work with xml formatted text (passed to PlistBuddy via stdin)
+		# "print_xml" - returns xml formatted text
+		# "print" - returns PlistBuddy's standard descriptive text format
+	# $2 = (str) Path to plist or generated xml
+	# $3 = (str) Key or key path to read
+	# $4 = (str) Type that will be used for the value
+	# $5 = (str) Value to be set to the passed key
+
+	local action="${1}"
+	local plist="${2}"
+	local key="${3}"
+	local type="${4}"
+	local value="${5}"
+
+	# Delete existing values if required
+	case "${action}" in
+		"print_xml" )
+			/usr/libexec/PlistBuddy -x -c "Print :${key}" "${plist}" || \
+			/usr/libexec/PlistBuddy -x -c "Print" "${plist}"
+		;;
+		"print_stdin" )
+			/usr/libexec/PlistBuddy -c "Print :${key}" /dev/stdin <<< "${plist}" || \
+			/usr/libexec/PlistBuddy -c "Print" /dev/stdin <<< "${plist}"
+		;;
+		"print" )
+			/usr/libexec/PlistBuddy -c "Print :${key}" "${plist}" || \
+			/usr/libexec/PlistBuddy -c "Print" "${plist}"
+		;;
+		"add" )
+			# Configure values
+			/usr/libexec/PlistBuddy -c "Add :${key} ${type} ${value}" "${plist}" > /dev/null 2>&1 \
+			|| /usr/libexec/PlistBuddy -c "Set :${key} ${value}" "${plist}" 2>&1
+		;;
+		"delete" )
+			/usr/libexec/PlistBuddy -c "Delete :${key} ${type}" "${plist}" 2>&1
+		;;
+		"clear" )
+			/usr/libexec/PlistBuddy -c "clear ${type}" "${plist}" 2>&1
+		;;
+	esac
+
+}
+
 create_shortcut() {
 
-	echo -e "\nProvided configuration:\n\t * Filename:  ${file_name}\n\t * URL:  ${URL} \
-		\n\t * Icon:  ${icon}\n\t * Location:  ${location}\n\nCreating the shortcut:"
+	console_writer "\nProvided configuration:\n\t * Filename:  ${file_name}\n\t * URL:  ${URL} \
+		\n\t * Icon:  ${icon}\n\t * Location:  ${location}\n\nCreating shortcut:" "-e"
 
 	# Create the staging directory if it doesn't existing.
 	if [[ ! -d "${default_location}" ]]; then
@@ -72,17 +159,18 @@ create_shortcut() {
 	fi
 
 	# Create the shortcut file.
+	console_writer "\t * Staging file..." "-e"
 	/usr/bin/printf '%s\n' "[InternetShortcut]" "URL=${URL}" "" > "${default_location}/${file_name}"
 
 	# If the icon provided is on a web server, download it.
-	if [[ "${icon}" =~ http* ]]; then
-		echo -e "\t * Downloading icon..."
+	if [[ "${icon}" =~ [Hh][Tt][Tt][Pp]* ]]; then
+		console_writer "\t * Downloading icon..." "-e"
 		/usr/bin/curl --silent --show-error --fail "${icon}"  --output /tmp/icon.png
 		icon="/tmp/icon.png"
 	fi
 
 	# Set the icon on the shortcut file.
-	echo -e "\t * Adding icon to shortcut..."
+	console_writer "\t * Adding icon to shortcut..." "-e"
 	"${python_binary}" -c "\
 import Cocoa, sys
 Cocoa.NSWorkspace.sharedWorkspace().setIcon_forFile_options_(Cocoa.NSImage.alloc().\
@@ -91,95 +179,165 @@ initWithContentsOfFile_(sys.argv[1]), sys.argv[2], 0) or sys.exit(\"Unable to se
 
 }
 
+check_shortcut_exists() {
+
+	# Check if the shortcut exists
+	# Results values:
+		# false = Does not Exist
+		# true = Exists
+	# Exit/Return code:
+		# (int) = the index of the shortcut in the `persistent-others` array
+
+	case "${location}" in
+		"Dock" )
+			console_writer "Determine if shortcut exists in the Dock..."
+
+			# Get the number of items in the persistent-others node.
+			index_item=$( PlistBuddy_Helper "print_xml" "${dock_plist}" ":persistent-others" | \
+			/usr/bin/xmllint --format - | xpath_tool 'count(//plist/array/dict )' 2>/dev/null )
+
+			# Loop through all the items in the persistent-others node
+			# and determine if the shortcut has already been added.
+			for ((i=0; i<$index_item; ++i)); do
+				index_label=$( PlistBuddy_Helper "print" "${dock_plist}" \
+					":persistent-others:${i}:tile-data:file-label" )
+				index_data=$( PlistBuddy_Helper "print" "${dock_plist}" \
+					":persistent-others:${i}:tile-data:file-data:_CFURLString" )
+
+				# Check if the current index_item values equal the script parameter values.
+				if [[ "${index_label}" == "${file_name}" &&
+					"${index_data}" == "file://${default_location}/$( echo ${file_name} | \
+					/usr/bin/sed 's/ /%20/g' )"
+				]]; then
+					echo "true"
+					return "${i}"
+				fi
+			done
+		;;
+		"Desktop" )
+			console_writer "Determine if shortcut exists on the Desktop..."
+			if [[ -e "/Users/${current_user}/Desktop/${file_name}" ]]; then
+				echo "true"
+				return
+			fi
+		;;
+		esac
+
+		console_writer "\t * Shortcut does not exist" "-e"
+		echo "false"
+}
+
+remediate_shortcut() {
+
+	# Remediate shortcut
+	console_writer "\t * Removing shortcut" "-e"
+
+	case "${location}" in
+		"Desktop" )
+			/bin/rm "/Users/${current_user}/Desktop${file_name}"
+		;;
+		"Dock" )
+			PlistBuddy_Helper "delete" "${dock_plist}" ":persistent-others:${index_match}" "dict"
+		;;
+	esac
+
+}
+
 ##################################################
 # Bits staged...
 
-# Check to make sure all parameters were provided.
-if [[ -z "${file_name}" && -z "${URL}" && -z "${icon}" && -z "${location}" ]]; then
-	echo "ERROR:  Missing required parameters!"
-	# Function get_help
-	get_help
-	echo -e "\n*****  Create Shortcut process:  FAILED  *****"
-	exit 1
-fi
+console_writer "*****  Create Shortcut process:  START  *****\n\n" "-e"
 
-echo ""
-# Check where to place the file.
-case "${location}" in
-	Dock )
-		echo "Determine if requested shortcut exists in the Dock..."
-		# Track whether the Dock item already exists; if it does,
-		# do not unnecessarily edit and kill the Dock.
-		already_exists=0
+file_name="${4}.url"
+URL="${5}"
+icon="${6}"
 
-		# Get the number of items in the persistent-others node.
-		index_item=$( /usr/libexec/PlistBuddy -x -c "Print :persistent-others" \
-			"/Users/${current_user}/Library/Preferences/com.apple.dock.plist" | \
-			/usr/bin/xmllint --format - | xpath_tool 'count(//plist/array/dict )' 2>/dev/null)
-		# Then subtract one for array value notation.
-		index_item=$((index_item-1))
+# Turn on case-insensitive pattern matching
+shopt -s nocasematch
 
-		# Loop through all the items in the persistent-others node
-		# and determine if the shortcut has already been added.
-		for ((i=0; i<=$index_item; ++i)); do
-			index_label=$( /usr/libexec/PlistBuddy -c "Print \
-				:persistent-others:${i}:tile-data:file-label" \
-				"/Users/${current_user}/Library/Preferences/com.apple.dock.plist" )
-			index_data=$( /usr/libexec/PlistBuddy -c "Print \
-				:persistent-others:${i}:tile-data:file-data:_CFURLString" \
-				"/Users/${current_user}/Library/Preferences/com.apple.dock.plist" )
-
-			# Check if the current index_item values equal the new items' values.
-			if [[ "${index_label}" == "${file_name}" &&
-				"${index_data}" == "file://${default_location}/$( echo ${file_name} | \
-				/usr/bin/sed 's/ /%20/g' )"
-			]]; then
-				already_exists=1
-				echo -e "\t * Shortcut already exists!"
-				break
-			fi
-		done
-
-		if [[ $already_exists == 0 ]]; then
-			echo -e "\t * Shortcut does not exist"
-
-			# Function create_shortcut
-			create_shortcut
-
-			echo -e "\t * Adding to the Dock..."
-			file_name_location=$( echo "${file_name}" | /usr/bin/sed 's/ /%20/g' )
-			/usr/bin/sudo -s -u "${current_user}" /usr/bin/defaults write \
-				"/Users/${current_user}/Library/Preferences/com.apple.dock" persistent-others \
-				-array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>\
-_CFURLString</key><string>file://${default_location}/${file_name_location}</string>\
-<key>_CFURLStringType</key><integer>15</integer></dict><key>file-label</key>\
-<string>${file_name}</string><key>file-type</key><integer>32</integer></dict>\
-<key>tile-type</key><string>file-tile</string></dict>"
-			/usr/bin/killall Dock
-
-		fi
+case "${7}" in
+	"Dock" )
+		location="Dock"
+		dock_plist="/Users/${current_user}/Library/Preferences/com.apple.dock.plist"
 	;;
-	Desktop )
-		echo "Determine if requested shortcut exists on the Desktop..."
-
-		if [[ -e "/Users/${current_user}/Desktop/${file_name}" ]]; then
-			echo -e "\t * Shortcut already exists!"
-		else
-			# Function create_shortcut
-			create_shortcut
-
-			# Move shortcut to the Desktop.
-			echo "Adding to the Desktop..."
-			/bin/mv "${default_location}/${file_name}" "/Users/${current_user}/Desktop"
-		fi
+	"Desktop" )
+		location="Desktop"
 	;;
 	* )
-		echo "ERROR:  The specified location is not configurable at this time."
-		# Function get_help
-		get_help
-		echo -e "\n*****  Create Shortcut process:  FAILED  *****"
+		handle_exit 1 "ERROR:  The specified location is not supported."
 	;;
 esac
 
-echo -e "\n*****  Create Shortcut process:  COMPLETE  *****"
-exit 0
+case "${8}" in
+	"Remove" | "Delete" )
+		option="Remove"
+	;;
+	"Replace" )
+		option="Replace"
+	;;
+	"" )
+		# No option passed
+	;;
+	* )
+		handle_exit 2 "ERROR:  The specified option is not supported."
+	;;
+esac
+
+# Turn off case-insensitive pattern matching
+shopt -u nocasematch
+
+# Check to make sure all parameters were provided.
+if [[ -z "${file_name}" && -z "${URL}" && -z "${icon}" && -z "${location}" ]]; then
+	handle_exit 3 "ERROR:  Missing required parameters!"
+fi
+
+shortcut_exists=$( check_shortcut_exists )
+index_match=$?
+
+if [[ "${shortcut_exists}" == "true" ]]; then
+	console_writer "\t * Shortcut exists!" "-e"
+
+	if [[ -n "${option}" ]]; then
+		# Remediate as requested
+		remediate_shortcut
+	else
+		# Do not remediate
+		handle_exit 0
+	fi
+
+fi
+
+if [[ "${shortcut_exists}" == "true" && "${option}" == "Remove" ]]; then
+	/usr/bin/killall Dock
+
+elif [[ ( "${shortcut_exists}" == "false" || "${option}" == "Replace" )
+		&& "${option}" != "Remove"
+]]; then
+
+	# Function create_shortcut
+	create_shortcut
+
+	# Check where to place the file.
+	case "${location}" in
+		"Desktop" )
+		# Move shortcut to the Desktop.
+		console_writer "\nAdding to the Desktop..." "-e"
+		/bin/mv "${default_location}/${file_name}" "/Users/${current_user}/Desktop"
+	;;
+	"Dock" )
+		console_writer "\nAdding to the Dock..." "-e"
+		file_name_location=$( echo "${file_name}" | /usr/bin/sed 's/ /%20/g' )
+
+		/usr/bin/sudo -s -u "${current_user}" /usr/bin/defaults write \
+			"/Users/${current_user}/Library/Preferences/com.apple.dock" persistent-others \
+			-array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString\
+</key><string>file://${default_location}/${file_name_location}</string><key>_CFURLStringType</key>\
+<integer>15</integer></dict><key>file-label</key><string>${file_name}</string><key>file-type</key>\
+<integer>32</integer></dict><key>tile-type</key><string>file-tile</string></dict>"
+		/usr/bin/killall Dock
+	;;
+	esac
+
+fi
+
+handle_exit 0
