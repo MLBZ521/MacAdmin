7bf1ea2f16b960cb5e83f9b1574ac5d9e9998ed1
Zack T <MLBZ521@gmail.com>
2023-03-21 00:34:35 -0700

    Added Full Overview Queries for Groups

--- Jamf Pro/Reporting/Database Maintenance and SQL Queries/Queries-Group Configurations.sql
+++ Jamf Pro/Reporting/Database Maintenance and SQL Queries/Queries-Group Configurations.sql
@@ -79,6 +79,85 @@ WHERE
 ;
 
 
+-- Full descriptive overview of Computer Groups
+SELECT DISTINCT
+	IF(sites.site_name IS NULL, "None", sites.site_name) AS "Site",
+	computer_groups.computer_group_name AS "Name",
+	computer_groups.computer_group_id AS "ID",
+	IF(computer_groups.is_smart_group = "1", "Yes", "No") AS "Smart Group",
+	IF(
+		computer_groups.computer_group_id NOT IN (
+			SELECT target_id FROM policy_deployment WHERE target_type = 7
+				UNION ALL
+			SELECT target_id FROM os_x_configuration_profile_deployment WHERE target_type = 7
+				UNION ALL
+			SELECT target_id FROM restricted_software_deployment WHERE target_type = 7
+				UNION ALL
+			SELECT target_id FROM mac_app_deployment WHERE target_type = 7
+				UNION ALL
+			SELECT target_id FROM patch_deployment WHERE target_type = 7
+				UNION ALL
+			SELECT target_id FROM ibook_deployment WHERE target_type = 7
+				UNION ALL
+			SELECT target_id FROM self_service_plugin_deployment WHERE target_type = 7
+		)
+		AND
+		computer_groups.computer_group_name NOT IN (
+			SELECT criteria FROM smart_computer_group_criteria WHERE search_field = "Computer Group"
+		),
+	"True", "False") AS "Not Used",
+	IF(computer_groups.computer_group_id NOT IN (
+		SELECT computer_group_id
+		FROM computer_group_memberships
+		),
+	"True", "False") AS "No Members",
+	CASE
+		WHEN computer_groups.is_smart_group = "0" THEN "Static"
+		WHEN (
+			computer_groups.is_smart_group = "1"
+			AND computer_groups.computer_group_id NOT IN (
+				SELECT computer_group_id
+				FROM smart_computer_group_criteria
+			)
+		) THEN "True"
+		Else "False"
+	END AS "No Defined Criteria",
+	CASE
+		WHEN computer_groups.is_smart_group = "0" THEN "Static"
+		WHEN (
+			computer_groups.computer_group_id IN (
+				SELECT computer_group_id
+				FROM smart_computer_group_criteria
+				GROUP BY computer_group_id
+				HAVING COUNT(computer_group_id) > 10
+			)
+		) THEN "True"
+		Else "False"
+	END AS "11+ Criteria",
+	CASE
+		WHEN computer_groups.is_smart_group = "0" THEN "Static"
+		WHEN (
+			computer_groups.computer_group_id IN (
+				SELECT computer_group_id
+				FROM smart_computer_group_criteria
+				WHERE search_field = "Computer Group"
+				GROUP BY computer_group_id
+				HAVING COUNT(computer_group_id) > 4
+			)
+		) THEN "True"
+		Else "False"
+	END AS "5+ Nested Groups"
+FROM computer_groups
+LEFT JOIN site_objects
+	ON computer_groups.computer_group_id = site_objects.object_id
+		AND site_objects.object_type = "7"
+LEFT JOIN sites
+	ON sites.site_id = site_objects.site_id
+LEFT JOIN smart_computer_group_criteria
+	ON smart_computer_group_criteria.computer_group_id = computer_groups.computer_group_id
+;
+
+
 -- ##################################################
 -- Mobile Device Groups
 
@@ -148,4 +227,79 @@ WHERE
 		SELECT mobile_device_group_id
 		FROM smart_mobile_device_group_criteria
 	)
+;
+
+
+-- Full descriptive overview of Mobile Device Groups
+SELECT DISTINCT
+	IF(sites.site_name IS NULL, "None", sites.site_name) AS "Site",
+	mobile_device_groups.mobile_device_group_name AS "Name",
+	mobile_device_groups.mobile_device_group_id AS "ID",
+	IF(mobile_device_groups.is_smart_group = "1", "Yes", "No") AS "Smart Group",
+	IF(
+		mobile_device_groups.mobile_device_group_id NOT IN (
+	SELECT target_id FROM mobile_device_configuration_profile_deployment WHERE target_type = 25
+		UNION ALL
+	SELECT target_id FROM mobile_device_app_deployment WHERE target_type = 25
+		UNION ALL
+	SELECT target_id FROM ibook_deployment WHERE target_type = 25
+		UNION ALL
+	SELECT mobile_device_group_id FROM classroom_mobile_device_group
+		)
+		AND
+		mobile_device_groups.mobile_device_group_name NOT IN (
+			SELECT criteria FROM smart_mobile_device_group_criteria WHERE search_field = "Mobile Device Group"
+		),
+	"True", "False") AS "Not Used",
+	IF(mobile_device_groups.mobile_device_group_id NOT IN (
+		SELECT mobile_device_group_id
+		FROM mobile_device_group_memberships
+		),
+	"True", "False") AS "No Members",
+
+	CASE
+		WHEN mobile_device_groups.is_smart_group = "0" THEN "Static"
+		WHEN (
+			mobile_device_groups.is_smart_group = "1"
+			AND mobile_device_groups.mobile_device_group_id NOT IN (
+				SELECT mobile_device_group_id
+				FROM smart_mobile_device_group_criteria
+			)
+		) THEN "True"
+		Else "False"
+	END AS "No Defined Criteria"
+
+	CASE
+		WHEN mobile_device_groups.is_smart_group = "0" THEN "Static"
+		WHEN (
+			mobile_device_groups.mobile_device_group_id IN (
+				SELECT mobile_device_group_id
+				FROM smart_mobile_device_group_criteria
+				GROUP BY mobile_device_group_id
+				HAVING COUNT(mobile_device_group_id) > 10
+			)
+		) THEN "True"
+		Else "False"
+	END AS "11+ Criteria"
+	CASE
+		WHEN mobile_device_groups.is_smart_group = "0" THEN "Static"
+		WHEN (
+			mobile_device_groups.mobile_device_group_id IN (
+				SELECT mobile_device_group_id
+				FROM smart_mobile_device_group_criteria
+				WHERE search_field = "Mobile Device Group"
+				GROUP BY mobile_device_group_id
+				HAVING COUNT(mobile_device_group_id) > 4
+			)
+		) THEN "True"
+		Else "False"
+	END AS "5+ Nested Groups"
+FROM mobile_device_groups
+LEFT JOIN site_objects
+	ON mobile_device_groups.mobile_device_group_id = site_objects.object_id
+		AND site_objects.object_type = "25"
+LEFT JOIN sites
+	ON sites.site_id = site_objects.site_id
+LEFT JOIN smart_mobile_device_group_criteria
+	ON mobile_device_groups.mobile_device_group_id = smart_mobile_device_group_criteria.mobile_device_group_id
 ;
