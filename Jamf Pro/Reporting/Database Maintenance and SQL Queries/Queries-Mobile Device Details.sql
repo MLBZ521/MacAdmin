fcbc76e5e4b2eec547492a7f5d8385cffcac8809
Zack T <MLBZ521@gmail.com>
2024-02-07 18:24:40 -0700

    Update and future proof DDM support logic

--- Jamf Pro/Reporting/Database Maintenance and SQL Queries/Queries-Mobile Device Details.sql
+++ Jamf Pro/Reporting/Database Maintenance and SQL Queries/Queries-Mobile Device Details.sql
@@ -190,11 +190,11 @@ SELECT
 		), "True", "False"
 	) AS "Failed to Renew MDM Profile",
 	IF(mobile_devices_denormalized.is_supervised = 1, "True", "False") AS "Supervised",
-	IF(
-		mobile_devices_denormalized.declarative_device_management_enabled = 1
-		and mobile_devices_denormalized.os_version LIKE "16.%",
-		"True", "False"
-	) AS "Declarative Device Management",
+	CASE
+		WHEN mobile_devices_denormalized.declarative_device_management_enabled = 1 THEN "True"
+		WHEN mobile_devices_denormalized.os_version REGEXP "^([1-9]|1[0-5])\.[.\d]+$" THEN "Not Supported"
+		ELSE "False"
+	END AS "Declarative Device Management",
 	IF(mobile_devices.user_removed_mdm_profile = 1, "True", "False") AS "Missing MDM Profile",
 	IF(mobile_devices_denormalized.mdm_profile_removable = 1, "True", "False") AS "MDM Profile Is Removable"
 FROM mobile_devices
